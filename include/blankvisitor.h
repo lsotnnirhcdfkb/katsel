5e7c967eb1784ff959720abbac9db6c7b8ca0b45
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-08-23 21:27:34 -0400

    add missing files

---
+++ include/blankvisitor.h
@@ -0,0 +1,20 @@
+#include "visitor.h"
+
+class BlankVisitor : public Visitor
+{
+public:
+    void visitBinaryAST(const BinaryAST *ast) override;
+    void visitTernaryOpAST(const TernaryOpAST *ast) override;
+    void visitUnaryAST(const UnaryAST *ast) override;
+    void visitPrimaryAST(const PrimaryAST *ast) override;
+    void visitExprStmtAST(const ExprStmtAST *ast) override;
+    void visitProgramAST(const ProgramAST *ast) override;
+    void visitFunctionAST(const FunctionAST *ast) override;
+    void visitBlockAST(const BlockAST *ast) override;
+    void visitTypeAST(const TypeAST *ast) override;
+    void visitArgAST(const ArgAST *ast) override;
+    void visitArgsAST(const ArgsAST *ast) override;
+    void visitVarStmtAST(const VarStmtAST *ast) override;
+    void visitAssignAST(const AssignAST *ast) override;
+    void visitVariableRefAST(const VariableRefAST *ast) override;
+};
---
+++ src/blankvisitor.cpp
@@ -0,0 +1,14 @@
+void BlankVisitor::visitBinaryAST(const BinaryAST *ast) {}
+void BlankVisitor::visitTernaryOpAST(const TernaryOpAST *ast) {}
+void BlankVisitor::visitUnaryAST(const UnaryAST *ast) {}
+void BlankVisitor::visitPrimaryAST(const PrimaryAST *ast) {}
+void BlankVisitor::visitExprStmtAST(const ExprStmtAST *ast) {}
+void BlankVisitor::visitProgramAST(const ProgramAST *ast) {}
+void BlankVisitor::visitFunctionAST(const FunctionAST *ast) {}
+void BlankVisitor::visitBlockAST(const BlockAST *ast) {}
+void BlankVisitor::visitTypeAST(const TypeAST *ast) {}
+void BlankVisitor::visitArgAST(const ArgAST *ast) {}
+void BlankVisitor::visitArgsAST(const ArgsAST *ast) {}
+void BlankVisitor::visitVarStmtAST(const VarStmtAST *ast) {}
+void BlankVisitor::visitAssignAST(const AssignAST *ast) {}
+void BlankVisitor::visitVariableRefAST(const VariableRefAST *ast) {}
