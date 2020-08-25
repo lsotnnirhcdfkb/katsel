8c1dcaca37312b7f55613637955682a8588e9896
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-08-25 18:04:16 -0400

    add python genall.py script to cmake with custsom target

--- utils/genall.py
+++ utils/genall.py
@@ -15,10 +15,9 @@ jobs = [
 ]
 
 for jobi, job in enumerate(jobs):
-    print(f'Running job {jobi + 1}/{len(jobs)}')
+    print(f'Running job {jobi + 1}/{len(jobs)}: insert {job[3]} to {job[0]}')
     with io.open(job[0], 'r', encoding='utf-8') as f:
         flines = f.readlines()
-        print(f'Loaded file {job[0]}');
 
     # delete old code
     genStartPattern = job[1]
@@ -30,7 +29,6 @@ for jobi, job in enumerate(jobs):
             print(f'"{genStartPattern}" found {len(genStart)} times in {job[0]}. Skipping {job[0]}')
             continue
         genStart = genStart[0]
-        print(f'Found "{genStartPattern}" on line {genStart + 1}')
     else:
         genStart = -1
 
@@ -40,11 +38,9 @@ for jobi, job in enumerate(jobs):
             print(f'"{genEndPattern}" found {len(genEnd)} times in {job[0]}. Skipping {job[0]}')
             continue
         genEnd = genEnd[0]
-        print(f'Found "{genEndPattern}" on line {genEnd + 1}')
     else:
         genEnd = len(flines)
 
-    print(f'Deleted {genEnd - (genStart + 1)} lines')
     del flines[genStart + 1:genEnd]
 
     # create command to generate new code
@@ -57,15 +53,9 @@ for jobi, job in enumerate(jobs):
         print(f'Command "{command}" failed with exit code {process.returncode}')
         continue
 
-    print(f'Command "{command}" ran successfully')
-
     # take the output of the command and put it back into the file
     generated = process.stdout.decode('utf-8')
     flines.insert(genStart + 1, generated)
 
-    print('Inserted ' + str(generated.count("\n")) + ' lines into source')
-
     with io.open(job[0], 'w', encoding='utf-8') as f:
         f.write(''.join(flines)) # write in all the code
-        print(f'Wrote file {job[0]}');
-    print()
