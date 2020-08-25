fb379065c182d85c1d8e92ea492731a6b4cda15b
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-08-25 08:54:57 -0400

    merge visitorgen.py and astgen.py

--- utils/astgen.py
+++ utils/astgen.py
@@ -1,54 +1,43 @@
 #!/usr/bin/env python3
 import argparse, sys
 
-# constants and stuff {{{1
-# T at the beginning means types
-# these are the only types that the asts have for now
-TTOKEN = 0
-TVECTOR = 1
-TUPTR = 2
-
-# T(ype) to C(ode) Type
-TTOCTYPE = {
-    TTOKEN: 'Token',
-    TVECTOR: 'std::vector<std::unique_ptr<AST>>',
-    TUPTR: 'std::unique_ptr<AST>',
-}
-
-# ac class, where ac is short for ast class {{{1
-class ac:
+# class: AstClass {{{1
+class AstClass:
     def __init__(self, name, fields=[]):
         self.name = name + 'AST'
         self.fields = fields
 
-    # ac.printHFile {{{2
+    # AstClass.printHFile {{{2
     def printHFile(self):
-        # print(('class {name}'
+        output = []
 
-        print(f'class {self.name} : public AST')
-        print( '{')
-        print( 'public:')
-        print(f'    {self.name}({self.genConstructArgs()});')
-        print( '    void accept(Visitor *v) override;')
-        print( )
+        output.append(    f'class {self.name} : public AST\n')
+        output.append(     '{\n')
+        output.append(     'public:\n')
+        output.append(    f'    {self.name}({self.genConstructArgs()});\n')
+        output.append(     '    void accept(Visitor *v) override;\n')
+        output.append('\n')
         for field in self.fields:
-            print(f'    {field.printHFile(False)};')
-        print( '};')
-        print( '')
+            output.append(f'    {field.printHFile(False)};\n')
+        output.append(     '};\n')
+        output.append(     '\n')
 
-    # ac.printCppFile {{{2
+        return ''.join(output)
+
+    # AstClass.printCppFile {{{2
     def printCppFile(self):
-        print(f'{self.name}::{self.name}({self.genConstructArgs()})', end='')
+        output = []
+        output.append(f'{self.name}::{self.name}({self.genConstructArgs()})')
 
         initializerList = []
         constructorBody = []
 
         for field in self.fields:
-            if field.type_ == TTOKEN:
-                initializerList.append(f'{field.name}({field.name})');
-            elif field.type_ == TUPTR:
-                initializerList.append(f'{field.name}(std::move({field.name}))');
-            elif field.type_ == TVECTOR:
+            if field.type_ == AstField.TTOKEN:
+                initializerList.append(f'{field.name}({field.name})')
+            elif field.type_ == AstField.TUPTR:
+                initializerList.append(f'{field.name}(std::move({field.name}))')
+            elif field.type_ == AstField.TVECTOR:
                 constructorBody.append((
                             f'    this->{field.name}.reserve({field.name}.size());\n'
                             f'    for (std::unique_ptr<AST> &ast : {field.name})\n'
@@ -58,23 +47,27 @@ class ac:
                             ))
 
         if len(initializerList):
-            print(f': {", ".join(initializerList)}', end='')
+            output.append(f': {", ".join(initializerList)}')
 
         if len(constructorBody):
-            print('\n{\n' + "\n".join(constructorBody) + '}')
+            output.append('\n{\n' + "\n".join(constructorBody) + '}\n')
         else:
-            print(' {}')
+            output.append(' {}\n')
 
-        print(f'void {self.name}::accept(Visitor *v) {{ v->visit{self.name}(this); }}\n')
+        output.append(f'void {self.name}::accept(Visitor *v) {{ v->visit{self.name}(this); }}\n\n')
+        return ''.join(output)
 
-    # ac helper methods {{{2
+    # AstClass helper methods {{{2
     def genConstructArgs(self):
         return ", ".join(f.printHFile(True) for f in self.fields)
 
-# abc class, where abc is short for ast base class {{{1
-class abc:
+# class: AstBaseClass {{{1
+class AstBaseClass:
+    def __init__(self):
+        self.name = 'AST'
+
     def printHFile(self):
-        print((
+        return (
         "class AST\n"
         "{\n"
         "public:\n"
@@ -82,81 +75,106 @@ class abc:
         "\n"
         "    virtual void accept(Visitor *v) = 0;\n"
         "};\n"
-        ))
-
+        )
+    
     def printCppFile(self):
-        pass
-        
-# af class, where af is short for ast field {{{1
-class af:
+        return ''
+
+# class: AstField {{{1
+class AstField:
+    TTOKEN = 0
+    TVECTOR = 1
+    TUPTR = 2
+
+    # T(ype) to C(ode) Type
+    TTOCTYPE = {
+        TTOKEN: 'Token',
+        TVECTOR: 'std::vector<std::unique_ptr<AST>>',
+        TUPTR: 'std::unique_ptr<AST>',
+    }
+
     def __init__(self, name, type_):
         self.name = name
         self.type_ = type_
 
     def printHFile(self, isArgPrint):
-        if isArgPrint and self.type_ == TVECTOR:
-            return f'{TTOCTYPE[self.type_]} &{self.name}'
+        if isArgPrint and self.type_ == AstField.TVECTOR:
+            return f'{AstField.TTOCTYPE[self.type_]} &{self.name}'
         else:
-            return f'{TTOCTYPE[self.type_]} {self.name}'
+            return f'{AstField.TTOCTYPE[self.type_]} {self.name}'
 
-    def printCppFile(self):
-        pass
+# generation functions: ast.h ast.cpp files {{{1
+# print ast.h file {{{2
+def printAstHFile():
+    for class_ in astClasses:
+        print(class_.printHFile())
 
-# print .h file driver code {{{1
-def printHFile():
-    print('#pragma once\n')
-    for include in includes:
-        print(f'#include {include}')
-
-    print()
-
-    for class_ in classes:
-        class_.printHFile()
-
-# print .cpp file driver code {{{1
-def printCppFile():
+# print ast.cpp file {{{2
+def printAstCppFile():
     print('#include "ast.h"')
     print()
-    for class_ in classes:
-        class_.printCppFile()
+    for class_ in astClasses:
+        print(class_.printCppFile())
 
-# include files for header file {{{1
-includes = [
-        '<vector>',
-        '<string>',
-        '<iostream>',
-        '<memory>',
-        '"token.h"',
-        '"visitor.h"'
-    ]
+# generation functions: visitor files {{{1
+# generate ast forward declarations for visitor.h {{{2
+def printForwardDecl():
+    for astClass in astClasses:
+        print(f'class {astClass.name};')
+# generate visitAST methods {{{2
+def printVisitASTMethods(isBase):
+    for astClass in astClasses:
+        if astClass == '':
+            continue
 
-# classes to generate {{{1
-classes = [
-    abc(),
-    ac('Binary'       , [af('op', TTOKEN), af('last', TUPTR), af('rast', TUPTR)]),
-    ac('TernaryOp'    , [af('conditional', TUPTR), af('trueast', TUPTR), af('falseast', TUPTR)]),
-    ac('Unary'        , [af('op', TTOKEN), af('ast', TUPTR)]),
-    ac('Primary'      , [af('value', TTOKEN)]),
-    ac('ExprStmt'     , [af('ast', TUPTR)]),
-    ac('Program'      , [af('asts', TVECTOR)]),
-    ac('Function'     , [af('type', TUPTR), af('name', TTOKEN), af('params', TUPTR), af('body', TUPTR)]),
-    ac('Block'        , [af('stmts', TVECTOR)]),
-    ac('Type'         , [af('type', TTOKEN)]),
-    ac('Param'        , [af('type', TUPTR), af('paramname', TTOKEN)]),
-    ac('Params'       , [af('params', TVECTOR)]),
-    ac('VarStmt'      , [af('type', TUPTR), af('name', TTOKEN), af('expression', TUPTR)]),
-    ac('Assign'       , [af('lhs', TUPTR), af('rhs', TUPTR), af('equalSign', TTOKEN)]),
-    ac('VariableRef'  , [af('var', TTOKEN)]),
-    ac('ReturnStmt'   , [af('expr', TUPTR)]),
-    ac('Arg'          , [af('expr', TUPTR)]),
-    ac('Args'         , [af('args', TVECTOR)]),
-    ac('Call'         , [af('varrefast', TUPTR), af('arglistast', TUPTR)]),
+        if isBase:
+            print('    virtual ', end='')
+        else:
+            print('    ', end='')
+
+        print(f'void visit{astClass.name}(const {astClass.name} *ast)', end='')
+
+        if isBase:
+            print(' = 0;')
+        else:
+            print(' override;')
+# generate BlankVisitor method definitions {{{2
+def printBlankVisitorDefinitions():
+    for astClass in astClasses:
+        if astClass == '':
+            continue
+        print(f'void BlankVisitor::visit{astClass}AST(const {astClass}AST *ast) {{}}')
+# lists: ast classes to generate {{{1
+astClasses = [
+    AstBaseClass(),
+    AstClass('Binary'       , [AstField('op', AstField.TTOKEN), AstField('last', AstField.TUPTR), AstField('rast', AstField.TUPTR)]),
+    AstClass('TernaryOp'    , [AstField('conditional', AstField.TUPTR), AstField('trueast', AstField.TUPTR), AstField('falseast', AstField.TUPTR)]),
+    AstClass('Unary'        , [AstField('op', AstField.TTOKEN), AstField('ast', AstField.TUPTR)]),
+    AstClass('Primary'      , [AstField('value', AstField.TTOKEN)]),
+    AstClass('ExprStmt'     , [AstField('ast', AstField.TUPTR)]),
+    AstClass('Program'      , [AstField('asts', AstField.TVECTOR)]),
+    AstClass('Function'     , [AstField('type', AstField.TUPTR), AstField('name', AstField.TTOKEN), AstField('params', AstField.TUPTR), AstField('body', AstField.TUPTR)]),
+    AstClass('Block'        , [AstField('stmts', AstField.TVECTOR)]),
+    AstClass('Type'         , [AstField('type', AstField.TTOKEN)]),
+    AstClass('Param'        , [AstField('type', AstField.TUPTR), AstField('paramname', AstField.TTOKEN)]),
+    AstClass('Params'       , [AstField('params', AstField.TVECTOR)]),
+    AstClass('VarStmt'      , [AstField('type', AstField.TUPTR), AstField('name', AstField.TTOKEN), AstField('expression', AstField.TUPTR)]),
+    AstClass('Assign'       , [AstField('lhs', AstField.TUPTR), AstField('rhs', AstField.TUPTR), AstField('equalSign', AstField.TTOKEN)]),
+    AstClass('VariableRef'  , [AstField('var', AstField.TTOKEN)]),
+    AstClass('ReturnStmt'   , [AstField('expr', AstField.TUPTR)]),
+    AstClass('Arg'          , [AstField('expr', AstField.TUPTR)]),
+    AstClass('Args'         , [AstField('args', AstField.TVECTOR)]),
+    AstClass('Call'         , [AstField('varrefast', AstField.TUPTR), AstField('arglistast', AstField.TUPTR)]),
 ]
 
-# actually running the things by parsing args and stuff {{{1
+# entry: actually running the things by parsing args and stuff {{{1
 parser = argparse.ArgumentParser(description='Generate AST classes.')
-parser.add_argument('-e', '--header', action='store_true', help='Generate header file of all the AST classes')
-parser.add_argument('-c', '--source', action='store_true', help='Generate source file of all the AST classes')
+parser.add_argument('--astheader', action='store_true', help='Generate ast.h classes')
+parser.add_argument('--astsource', action='store_true', help='Generate ast.cpp')
+parser.add_argument('--forwarddecl', action='store_true', help='Generate forward declarations for visitor.h')
+parser.add_argument('--visitormethods', action='store_true', help='Generate visitSomethingAST methods')
+parser.add_argument('--visitorbasemethods', action='store_true', help='Generate visitSomethingAST methods in the pure virtual Visitor class')
+parser.add_argument('--blankvisitor', action='store_true', help='Generate BlankVisitor::visitSomethingAST method definitions')
 
 if len(sys.argv) == 1:
     parser.print_help(sys.stderr)
@@ -164,9 +182,20 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-if args.header:
-    printHFile()
+if args.astheader:
+    printAstHFile()
 
-if args.source:
-    printCppFile()
+if args.astsource:
+    printAstCppFile()
 
+if args.forwarddecl:
+    printForwardDecl()
+
+if args.visitormethods:
+    printVisitASTMethods(False)
+
+if args.visitorbasemethods:
+    printVisitASTMethods(True)
+
+if args.blankvisitor:
+    printBlankVisitorDefinitions()
