67570ce7f8df6ab3afb5c584a9130dfb9771b19e
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-08-24 22:40:35 -0400

    add more messages to genall.py

--- utils/genall.py
+++ utils/genall.py
@@ -9,9 +9,11 @@ jobs = [
     ('include/visitor.h', None              , None            , 'utils/visitorincludegen.py' , '')
 ]
 
-for job in jobs:
+for jobi, job in enumerate(jobs):
+    print(f'Running job {jobi}/{len(jobs)}')
     with io.open(job[0], 'r', encoding='utf-8') as f:
         flines = f.readlines()
+        print(f'Loaded file {job[0]}');
 
     # delete old code
     genStartPattern = job[1]
@@ -23,6 +25,7 @@ for job in jobs:
             print(f'"{genStartPattern}" found {len(genStart)} times in {job[0]}. Skipping {job[0]}')
             continue
         genStart = genStart[0]
+        print(f'Found "{genStartPattern}" on line {genStart + 1}')
     else:
         genStart = -1
 
@@ -32,9 +35,11 @@ for job in jobs:
             print(f'"{genEndPattern}" found {len(genEnd)} times in {job[0]}. Skipping {job[0]}')
             continue
         genEnd = genEnd[0]
+        print(f'Found "{genEndPattern}" on line {genEnd + 1}')
     else:
         genEnd = len(flines)
 
+    print(f'Deleted {genEnd - (genStart + 1)} lines')
     del flines[genStart + 1:genEnd]
 
     # create command to generate new code
@@ -47,8 +52,15 @@ for job in jobs:
         print(f'Command "{command}" failed with exit code {process.returncode}')
         continue
 
+    print(f'Command "{command}" ran successfully')
+
     # take the output of the command and put it back into the file
-    flines.insert(genStart + 1, process.stdout.decode('utf-8'))
+    generated = process.stdout.decode('utf-8')
+    flines.insert(genStart + 1, generated)
+
+    print('Inserted ' + str(generated.count("\n")) + ' lines into source')
 
     with io.open(job[0], 'w', encoding='utf-8') as f:
         f.write(''.join(flines)) # write in all the code
+        print(f'Wrote file {job[0]}');
+    print()
