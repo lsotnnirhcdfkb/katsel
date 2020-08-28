63eb376037596465f7e56cd4d6adc67929067688
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-08-28 16:13:46 -0400

    document the token struct

--- include/token.h
+++ include/token.h
@@ -5,13 +5,19 @@
 /// A Token emitted from the lexer lexing the source file
 struct Token
 {
+    /// The type of this token
     TokenType type;
+    /// An iterator to where this token starts in the source
     std::string::iterator start;
+    /// An iterator to where this token ends in the source
     std::string::iterator end;
 
+    /// The error message if this token is an error token
     std::string message;
 
+    /// The token's line number
     int line;
+    /// The token's column number
     int column;
 };
 
