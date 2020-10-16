213418deafcbd45b2f5c7604a225c0ad03650113
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-10-16 13:26:22 -0400

    add statements but not really

--- src/codegen/stmt/codegenstmt.cpp
+++ src/codegen/stmt/codegenstmt.cpp
@@ -2,17 +2,21 @@
 
 void CodeGen::visitBlockStmt(ASTNS::BlockStmt *a)
 {
-	
+	context.incScope();
+    for (std::unique_ptr<ASTNS::Stmt> &s : a->stmts)
+        s->accept(this);
+	context.decScope();
 }
 void CodeGen::visitExprStmt(ASTNS::ExprStmt *a)
 {
-	
+	a->expr->accept(this);
 }
 void CodeGen::visitReturnStmt(ASTNS::ReturnStmt *a)
 {
-	
+	// TODO: this
 }
+
 void CodeGen::visitVarStmt(ASTNS::VarStmt *a)
 {
-	
+	// TODO: this
 }
