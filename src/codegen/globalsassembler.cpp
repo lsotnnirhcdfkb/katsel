3fc8c327abbcfe9a467a4346cf0d7c5a9ecffe57
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-11-13 00:34:31 -0500

    remove globalsassembler code

--- src/codegen/globalsassembler.cpp
+++ src/codegen/globalsassembler.cpp
@@ -8,46 +8,46 @@
 
 GlobalsAssembler::GlobalsAssembler(CodeGenContext &con, CodeGen &codeGen): context(con), codeGen(codeGen) {}
 
-void GlobalsAssembler::visitDecls(ASTNS::Decls *a)
-{
-    a->decls->accept(this);
-    a->decl->accept(this);
-}
-
-void GlobalsAssembler::visitFunction(ASTNS::Function *a)
-{
-    std::string fnamestr (a->name.stringify());
-    Value declbefore = context.getGlobal(fnamestr);
-    if (declbefore.val)
-    {
-        Error(Error::MsgType::ERROR, a->name, "Duplicate function")
-            .underline(Error::Underline(a->name, '^')
-                .error("Duplciate function"))
-            .underline(Error::Underline(declbefore, '-')
-                .note("Previous declaration is here"))
-            .report();
-        return;
-    }
-
-    Type *ret = codeGen.evalType(a->retty.get());
-
-    std::vector<CodeGen::Param> params;
-    if (a->form == ASTNS::Function::Form::TATTATA)
-        params = codeGen.evalParams(a->paramlist.get());
-
-    std::vector<llvm::Type*> paramtysasllvm;
-    std::vector<Type*> paramtys;
-
-    for (CodeGen::Param p : params)
-    {
-        paramtys.push_back(p.ty);
-        paramtysasllvm.push_back(p.ty->toLLVMType(context.context));
-    }
-
-    llvm::Type *retTyllvm = ret->toLLVMType(context.context);
-    llvm::FunctionType *ft = llvm::FunctionType::get(retTyllvm, paramtysasllvm, false);
-    llvm::Function *f = llvm::Function::Create(ft, llvm::Function::ExternalLinkage, a->name.stringify(), context.mod.get());
-
-    context.globalSymbolTable[fnamestr] = Value(context.getFunctionType(ret, paramtys), f, a);
-}
-
+// void GlobalsAssembler::visitDecls(ASTNS::Decls *a)
+// {
+//     a->decls->accept(this);
+//     a->decl->accept(this);
+// }
+// 
+// void GlobalsAssembler::visitFunction(ASTNS::Function *a)
+// {
+//     std::string fnamestr (a->name.stringify());
+//     Value declbefore = context.getGlobal(fnamestr);
+//     if (declbefore.val)
+//     {
+//         Error(Error::MsgType::ERROR, a->name, "Duplicate function")
+//             .underline(Error::Underline(a->name, '^')
+//                 .error("Duplciate function"))
+//             .underline(Error::Underline(declbefore, '-')
+//                 .note("Previous declaration is here"))
+//             .report();
+//         return;
+//     }
+// 
+//     Type *ret = codeGen.evalType(a->retty.get());
+// 
+//     std::vector<CodeGen::Param> params;
+//     if (a->form == ASTNS::Function::Form::TATTATA)
+//         params = codeGen.evalParams(a->paramlist.get());
+// 
+//     std::vector<llvm::Type*> paramtysasllvm;
+//     std::vector<Type*> paramtys;
+// 
+//     for (CodeGen::Param p : params)
+//     {
+//         paramtys.push_back(p.ty);
+//         paramtysasllvm.push_back(p.ty->toLLVMType(context.context));
+//     }
+// 
+//     llvm::Type *retTyllvm = ret->toLLVMType(context.context);
+//     llvm::FunctionType *ft = llvm::FunctionType::get(retTyllvm, paramtysasllvm, false);
+//     llvm::Function *f = llvm::Function::Create(ft, llvm::Function::ExternalLinkage, a->name.stringify(), context.mod.get());
+// 
+//     context.globalSymbolTable[fnamestr] = Value(context.getFunctionType(ret, paramtys), f, a);
+// }
+// 
