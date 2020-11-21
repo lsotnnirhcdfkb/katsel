c06172013520be11dbd823c9be70b63bb2483549
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-11-21 18:46:34 -0500

    add statement codegen

--- src/codegen/stmt.cpp
+++ src/codegen/stmt.cpp
@@ -3,13 +3,61 @@
 
 CodeGenNS::StmtCodeGen::StmtCodeGen(CodeGen &cg): cg(cg) {}
 
-void CodeGenNS::StmtCodeGen::stmt(ASTNS::StmtB *ast) {}
+void CodeGenNS::StmtCodeGen::stmt(ASTNS::StmtB *ast)
+{
+    ast->accept(this);
+}
 
-void CodeGenNS::StmtCodeGen::visitBlock(ASTNS::Block *ast) {}
-void CodeGenNS::StmtCodeGen::visitExprStmt(ASTNS::ExprStmt *ast) {}
-void CodeGenNS::StmtCodeGen::visitRetStmt(ASTNS::RetStmt *ast) {}
-void CodeGenNS::StmtCodeGen::visitVarStmt(ASTNS::VarStmt *ast) {}
+void CodeGenNS::StmtCodeGen::visitBlock(ASTNS::Block *ast)
+{
+    cg.context.incScope();
+    if (ast->stmts)
+        ast->stmts->accept(this);
+    cg.context.decScope();
+}
+void CodeGenNS::StmtCodeGen::visitExprStmt(ASTNS::ExprStmt *ast)
+{
+    cg.exprCodeGen.expr(ast->expr.get());
+}
+void CodeGenNS::StmtCodeGen::visitRetStmt(ASTNS::RetStmt *ast)
+{
+    if (ast->expr)
+    {
+        Value *v = cg.exprCodeGen.expr(ast->expr.get());
+        if (!v)
+            return;
+
+        FunctionType *fty = static_cast<FunctionType*>(cg.context.curFunc->type());
+        if (fty->ret != v->type())
+        {
+            Error(Error::MsgType::ERROR, v, "cannot return value of different type than expected return value")
+                .underline(Error::Underline(v, '^')
+                    .error(concatMsg("returning ", v->type()->stringify(), " here")))
+                .underline(Error::Underline(static_cast<ASTNS::Function*>(cg.context.curFunc->ast())->retty.get(), '-')
+                    .note(concatMsg("function returns ", fty->ret->stringify())))
+                .report();
+            return;
+        }
+
+        // cg.context.builder.CreateRet(v.val); TODO
+    }
+    else
+        // cg.context.builder.CreateRetVoid();
+        return;
+}
+void CodeGenNS::StmtCodeGen::visitVarStmt(ASTNS::VarStmt *ast)
+{
+    Type *ty = cg.typeResolver.type(ast->type.get());
+
+    varty = ty;
+    ast->assignments->accept(this);
+    varty = nullptr;
+}
 
 void CodeGenNS::StmtCodeGen::visitEmptyStmt(ASTNS::EmptyStmt *) {}
-void CodeGenNS::StmtCodeGen::visitStmtList(ASTNS::StmtList *ast) {}
+void CodeGenNS::StmtCodeGen::visitStmtList(ASTNS::StmtList *ast)
+{
+    ast->stmtlist->accept(this);
+    ast->stmt->accept(this);
+}
 
