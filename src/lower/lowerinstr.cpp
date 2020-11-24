151ce3a8bf1a0e0efc0f23a83659ba8423649c4e
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-11-24 13:19:35 -0500

    start doing lowering instructions

--- src/lower/lowerinstr.cpp
+++ src/lower/lowerinstr.cpp
@@ -7,33 +7,46 @@ void Lower::Lowerer::visitStore(IR::Instrs::Store *instr)
     llvm::AllocaInst *target = allocas.at(instr->target);
     builder.CreateStore(lower(instr->value), target);
 }
-void Lower::Lowerer::visitAnd(IR::Instrs::And *instr) {}
-void Lower::Lowerer::visitOr(IR::Instrs::Or *instr) {}
-void Lower::Lowerer::visitIntCmpNE(IR::Instrs::IntCmpNE *instr) {}
+
+void Lower::Lowerer::visitIntCmpNE(IR::Instrs::IntCmpNE *instr) {} // TODO: signed and unsigned!!!
 void Lower::Lowerer::visitIntCmpEQ(IR::Instrs::IntCmpEQ *instr) {}
 void Lower::Lowerer::visitIntCmpULT(IR::Instrs::IntCmpULT *instr) {}
 void Lower::Lowerer::visitIntCmpUGT(IR::Instrs::IntCmpUGT *instr) {}
 void Lower::Lowerer::visitIntCmpULE(IR::Instrs::IntCmpULE *instr) {}
 void Lower::Lowerer::visitIntCmpUGE(IR::Instrs::IntCmpUGE *instr) {}
-void Lower::Lowerer::visitFloatCmpNE(IR::Instrs::FloatCmpNE *instr) {}
-void Lower::Lowerer::visitFloatCmpEQ(IR::Instrs::FloatCmpEQ *instr) {}
-void Lower::Lowerer::visitFloatCmpULT(IR::Instrs::FloatCmpULT *instr) {}
-void Lower::Lowerer::visitFloatCmpUGT(IR::Instrs::FloatCmpUGT *instr) {}
-void Lower::Lowerer::visitFloatCmpULE(IR::Instrs::FloatCmpULE *instr) {}
-void Lower::Lowerer::visitFloatCmpUGE(IR::Instrs::FloatCmpUGE *instr) {}
-void Lower::Lowerer::visitBitXor(IR::Instrs::BitXor *instr) {}
-void Lower::Lowerer::visitBitOr(IR::Instrs::BitOr *instr) {}
-void Lower::Lowerer::visitBitAnd(IR::Instrs::BitAnd *instr) {}
-void Lower::Lowerer::visitBitNot(IR::Instrs::BitNot *instr) {}
-void Lower::Lowerer::visitShiftR(IR::Instrs::ShiftR *instr) {}
-void Lower::Lowerer::visitShiftL(IR::Instrs::ShiftL *instr) {}
-void Lower::Lowerer::visitAdd(IR::Instrs::Add *instr) {}
-void Lower::Lowerer::visitSub(IR::Instrs::Sub *instr) {}
-void Lower::Lowerer::visitMult(IR::Instrs::Mult *instr) {}
-void Lower::Lowerer::visitDiv(IR::Instrs::Div *instr) {}
-void Lower::Lowerer::visitMod(IR::Instrs::Mod *instr) {}
-void Lower::Lowerer::visitNeg(IR::Instrs::Neg *instr) {}
-void Lower::Lowerer::visitTrunc(IR::Instrs::Trunc *instr) {}
+
+#define BASICBINARYOP(instrname, llvmname)                                                                              \
+    void Lower::Lowerer::visit##instrname(IR::Instrs::instrname *instr)                                                 \
+    {                                                                                                                   \
+        builder.CreateStore(builder.Create##llvmname(lower(instr->lhs), lower(instr->rhs)), allocas.at(instr->target)); \
+    }
+BASICBINARYOP(And, And)
+BASICBINARYOP(Or, Or)
+
+BASICBINARYOP(FloatCmpNE, FCmpONE)
+BASICBINARYOP(FloatCmpEQ, FCmpOEQ)
+BASICBINARYOP(FloatCmpLT, FCmpOLT)
+BASICBINARYOP(FloatCmpGT, FCmpOGT)
+BASICBINARYOP(FloatCmpLE, FCmpOLE)
+BASICBINARYOP(FloatCmpGE, FCmpOGE)
+
+BASICBINARYOP(BitXor, Xor)
+BASICBINARYOP(BitOr, Or)
+BASICBINARYOP(BitAnd, And)
+BASICBINARYOP(ShiftR, LShr)
+BASICBINARYOP(ShiftL, Shl)
+
+void Lower::Lowerer::visitAdd(IR::Instrs::Add *instr) {} // TODO: fadd and add
+void Lower::Lowerer::visitSub(IR::Instrs::Sub *instr) {} // TODO: sub and fsub
+void Lower::Lowerer::visitMul(IR::Instrs::Mul *instr) {} // TODO: mul and fmul
+void Lower::Lowerer::visitDiv(IR::Instrs::Div *instr) {} // TODO: udiv and sdiv and fdiv
+void Lower::Lowerer::visitMod(IR::Instrs::Mod *instr) {} // TODO: urem and srem and frem
+
+void Lower::Lowerer::visitNeg(IR::Instrs::Neg *instr) {} // TODO: fneg
+
+void Lower::Lowerer::visitTrunc(IR::Instrs::Trunc *instr) {} // TODO: trunc, fptrunc
+
+// TODO: these
 void Lower::Lowerer::visitZeroExt(IR::Instrs::ZeroExt *instr) {}
 void Lower::Lowerer::visitSignExt(IR::Instrs::SignExt *instr) {}
 void Lower::Lowerer::visitFloatTrunc(IR::Instrs::FloatTrunc *instr) {}
