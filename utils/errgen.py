d95546adb064b6c8acd4a848082588a90d2ab54b
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-11-26 17:33:20 -0500

    generate errmsgs.cpp file

--- utils/errgen.py
+++ utils/errgen.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import textwrap
+
 UNDER0 = '^'
 UNDER1 = '='
 UNDER2 = '~'
@@ -14,10 +16,11 @@ errors = [
     {
         'code': 1,
         'name': 'unexpected-char',
-        'desc': 'The lexer found an unexpected character that could not begin a token',
+        'desc': 'The lexer found an unexpected character that could not begin a token.',
         'inputs': 'Token const &tok',
+        'location': 'tok',
         'highlights': [
-            ('tok', UNDER0, ('error', 'unexpected character'))
+            ('tok', UNDER0, [('error', '\"unexpected character\"')])
         ]
     },
 ]
@@ -33,15 +36,32 @@ def genH():
     for error in errors:
         code = str(error['code']).zfill(PADAMT)
         output.append(f'// E{code} - {error["name"]}\n')
+        output.append(f'#define ERR_{error["name"].upper().replace("-", "_")} E{code}\n')
         output.append(f'void E{code}({error["inputs"]});\n')
         output.append( '\n')
-
-    for error in errors:
-        output.append(f'#define ERR_{error["name"].upper().replace("-", "_")} E{str(error["code"]).zfill(PADAMT)}\n')
 
     return ''.join(output)
 
 def genCpp():
     output = []
+
+    for error in errors:
+        code = str(error['code']).zfill(PADAMT)
+        output.append(        f'// E{code} - {error["name"]}\n')
+        descWrapped = "".join("// | " + line + "\n" for line in textwrap.wrap(error['desc'], 60))
+        output.append(        descWrapped)
+        output.append(        f'void E{code}({error["inputs"]})\n')
+        output.append(         '{\n')
+        output.append(        f'    Error(Error::MsgType::ERROR, {error["location"]}, "{error["name"]}")\n')
+
+        for loc, und, msgs in error['highlights']:
+            output.append(    f'        .underline(Error::Underline({loc}, \'{und}\')\n')
+            for ty, msg in msgs:
+                output.append(f'            .{ty}({msg})\n')
+            output.append(    '        )\n')
+
+        output.append(        '    ;\n')
+
+        output.append(         '}\n')
 
     return ''.join(output)
