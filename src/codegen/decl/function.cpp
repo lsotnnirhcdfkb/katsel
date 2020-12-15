1c192a2e7bdbe529c2fc6472f601e6f0a504b667
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-12-15 17:53:33 -0500

    do function codegen

--- src/codegen/decl/function.cpp
+++ src/codegen/decl/function.cpp
@@ -1,10 +1,82 @@
 #include "../codegenlocal.h"
 #include "utils/format.h"
+#include "message/errmsgs.h"
 
 CodeGen::FunctionCodeGen::FunctionCodeGen(CodeGen &cg, ASTNS::FunctionDecl *ast): curScope(0), cg(cg), ast(ast), exprCG(cg, *this), stmtCG(cg, *this) {}
 
 bool CodeGen::FunctionCodeGen::codegen()
 {
+    std::string name = ast->name.stringify();
+    IR::Value *function = cg.context->getGlobal(name);
+    IR::FunctionType *fty;
+    if (!(fty = dynamic_cast<IR::FunctionType*>(function->type())))
+        return false; // this does n ot happen in valid code, but this can happen if the user (erroenously) declares a variable and a function in the global namepsace with the same name, and the variable comes first so it gets chosen over the function
+
+    IR::Function *f = static_cast<IR::Function*>(function);
+
+    if (f->blocks.size() > 0)
+        return false;
+
+    if (!ast->body)
+    {
+        f->prototypeonly = true;
+        return true;
+    }
+
+    IR::Block *entryBlock = f->addBlock("entry");
+    exitBlock = f->addBlock("exit");
+
+    incScope();
+    ret = f->addRegister(fty->ret, ast);
+
+    if (ast->paramlist)
+    {
+        ParamVisitor pv (cg);
+        ast->paramlist->accept(&pv);
+        std::vector<ParamVisitor::Param> params (pv.ret);
+
+        for (auto const &param : params)
+        {
+            std::string pname = param.name;
+            IR::Register *reg = f->addRegister(param.ty, param.ast);
+
+            Local *foundparam = getLocal(pname);
+            if (foundparam)
+            {
+                ERR_REDECL_PARAM(param.ast->name, foundparam->v);
+                cg.errored = true;
+            }
+            else
+                addLocal(pname, reg);
+        }
+    }
+
+    fun = f;
+    curBlock = entryBlock;
+
+    IR::ASTValue retval = exprCG.expr(ast->body.get());
+
+    decScope();
+
+    if (!errored)
+    {
+        exitBlock->add(std::make_unique<IR::Instrs::Return>(ret));
+
+        if (ret->type() != retval.type())
+        {
+            ERR_CONFLICT_RET_TY(retval, f);
+            errored = true;
+        }
+        else
+        {
+            curBlock->add(std::make_unique<IR::Instrs::Store>(ret, retval));
+            curBlock->branch(std::make_unique<IR::Instrs::GotoBr>(exitBlock));
+        }
+    }
+
+    if (curScope != 0)
+        reportAbortNoh("At the end of FunctionCodeGen::codegen, curScope != 0");
+
     return !errored;
 }
 
