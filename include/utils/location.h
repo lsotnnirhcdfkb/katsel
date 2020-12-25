79f234ebe9918c371e26d6fadd28fba476553279
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-12-24 23:25:56 -0500

    add missing file

---
+++ include/utils/location.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+struct File;
+struct ASTen;
+namespace IR { struct ASTValue; }
+namespace ASTNS { class AST; }
+
+struct Location
+{
+    std::string::iterator start;
+    std::string::iterator end;
+    File const *file;
+
+    Location(Token const &t);
+    Location(std::string::iterator start, std::string::iterator end, File const *file);
+    Location(IR::ASTValue const &v);
+    Location(IR::ASTValue const *v);
+    Location(ASTNS::AST *ast);
+};
