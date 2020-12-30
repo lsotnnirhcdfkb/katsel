9e8e89b253259f8cfc33835f2540ef06fa3bf782
Jason Nguyen <hpj2ltxry43b@gmail.com>
2020-12-30 13:52:46 -0500

    fix returning void

--- src/lower/lowerbr.cpp
+++ src/lower/lowerbr.cpp
@@ -3,6 +3,8 @@
 #include "lower/lowerer.h"
 #include "ir/type.h"
 
+#include "llvm/IR/Constants.h"
+
 void Lower::Lowerer::visitGotoBr(IR::Instrs::GotoBr *instr)
 {
     builder.CreateBr(blocks.at(instr->to));
@@ -16,5 +18,5 @@ void Lower::Lowerer::visitReturn(IR::Instrs::Return *instr)
     if (!dynamic_cast<IR::VoidType*>(instr->value.type()))
         builder.CreateRet(lower(instr->value));
     else
-        builder.CreateRetVoid();
+        builder.CreateRet(llvm::ConstantStruct::get(llvm::StructType::get(context)));
 }
