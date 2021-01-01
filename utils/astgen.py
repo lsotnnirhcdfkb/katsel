051654d13899c53a8954322dda3d618027621d57
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-01-01 12:42:46 -0500

    rename ast classes in python script

--- utils/astgen.py
+++ utils/astgen.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 # Classes {{{1
-# ASTClass {{{2
-class ASTClass:
+# AST {{{2
+class AST:
     def __init__(self, name, base, fields):
         self.name = name
         def process_field(field):
             return (item.strip().rstrip() for item in field.split('|'))
         self.fields = [ASTField(*process_field(field)) for field in fields.split(',')]
         self.base = base
-# ASTBaseClass {{{2
-class ASTBaseClass:
+# ASTBase {{{2
+class ASTBase:
     def __init__(self, name):
         self.name = name
-# ASTSuperBaseClass {{{2
-class ASTSuperBaseClass:
+# ASTSuperBase {{{2
+class ASTSuperBase:
     def __init__(self):
         self.name ='AST'
 # ASTField {{{2
@@ -27,16 +27,16 @@ class ASTField:
         return self.type == other.type and self.name == other.name
 # Classes to generate {{{1
 asts = [
-    ASTSuperBaseClass(),
-    ASTBaseClass('CUB'),
-    ASTBaseClass('Decl'),
-    ASTBaseClass('Stmt'),
-    ASTBaseClass('Expr'),
-    ASTBaseClass('Type'),
-    ASTBaseClass('ArgB'),
-    ASTBaseClass('ParamB'),
-    ASTBaseClass('VStmtIB'),
-    ASTBaseClass('ImplRetB'),
+    ASTSuperBase(),
+    ASTBase('CUB'),
+    ASTBase('Decl'),
+    ASTBase('Stmt'),
+    ASTBase('Expr'),
+    ASTBase('Type'),
+    ASTBase('ArgB'),
+    ASTBase('ParamB'),
+    ASTBase('VStmtIB'),
+    ASTBase('ImplRetB'),
 
     # a class to keep track of locations of syntactic elements that don't matter (like
     # line endings where the location matters (so that other statements can use the line
@@ -44,37 +44,37 @@ asts = [
     # so its location cannot be kept track of otherwise)
     # it should never appear in the final AST, because no fields should have a PureLocationB
     # or a PureLocation as a field
-    ASTBaseClass('PureLocationB'),
-    ASTClass('PureLocation',  'PureLocationB', 'int|dummy'),
+    ASTBase('PureLocationB'),
+    AST('PureLocation',  'PureLocationB', 'int|dummy'),
 
-    ASTClass('CU',  'CUB', 'std::unique_ptr<DeclList>|decls'),
-    ASTClass('DeclList',  'Decl', 'std::vector<std::unique_ptr<Decl>>|decls'),
-    ASTClass('FunctionDecl', 'Decl', 'std::unique_ptr<Type>|retty, Token|name, std::unique_ptr<ParamList>|params, std::unique_ptr<Block>|body'),
-    ASTClass('VarStmt', 'Stmt', 'std::unique_ptr<VarStmtItemList>|assignments'),
-    ASTClass('VarStmtItem', 'VStmtIB', 'std::unique_ptr<Type>|type, Token|name, Token|equal, std::unique_ptr<Expr>|expr'),
-    ASTClass('VarStmtItemList', 'VStmtIB', 'std::vector<std::unique_ptr<VarStmtItem>>|items'),
-    ASTClass('ExprStmt', 'Stmt', 'std::unique_ptr<Expr>|expr'),
-    ASTClass('RetStmt', 'Stmt', 'std::unique_ptr<Expr>|expr'),
-    ASTClass('StmtList', 'Stmt', 'std::vector<std::unique_ptr<Stmt>>|stmts'),
-    ASTClass('ImplRet', 'ImplRetB', 'std::unique_ptr<Expr>|expr'),
-    ASTClass('PrimitiveType', 'Type', 'Token|ty'),
-    ASTClass('PointerType', 'Type', 'std::unique_ptr<Type>|type'),
-    ASTClass('Arg', 'ArgB', 'std::unique_ptr<Expr>|expr'),
-    ASTClass('ArgList', 'ArgB', 'std::vector<std::unique_ptr<Arg>>|args'),
-    ASTClass('Param', 'ParamB', 'std::unique_ptr<Type>|type, Token|name'),
-    ASTClass('ParamList', 'ParamB', 'std::vector<std::unique_ptr<Param>>|params'),
-    ASTClass('Block', 'Expr', 'std::unique_ptr<StmtList>|stmts, std::unique_ptr<ImplRet>|implRet'),
-    ASTClass('IfExpr', 'Expr', 'Token|iftok, std::unique_ptr<Expr>|cond, std::unique_ptr<Expr>|trues, std::unique_ptr<Expr>|falses'),
-    ASTClass('ForExpr', 'Expr', 'std::unique_ptr<VarStmt>|initial, std::unique_ptr<Expr>|cond, std::unique_ptr<Expr>|increment, std::unique_ptr<Expr>|body'),
-    ASTClass('AssignmentExpr', 'Expr', 'std::unique_ptr<Expr>|target, Token|equal, std::unique_ptr<Expr>|expr'),
-    ASTClass('ShortCircuitExpr', 'Expr', 'std::unique_ptr<Expr>|lhs, Token|op, std::unique_ptr<Expr>|rhs'),
-    ASTClass('BinaryExpr', 'Expr', 'std::unique_ptr<Expr>|lhs, Token|op, std::unique_ptr<Expr>|rhs'),
-    ASTClass('CastExpr', 'Expr', 'std::unique_ptr<Type>|type, std::unique_ptr<Expr>|expr'),
-    ASTClass('UnaryExpr', 'Expr', 'Token|op, std::unique_ptr<Expr>|expr'),
-    ASTClass('AddrofExpr', 'Expr', 'Token|op, std::unique_ptr<Expr>|expr'),
-    ASTClass('DerefExpr', 'Expr', 'Token|op, std::unique_ptr<Expr>|expr'),
-    ASTClass('CallExpr', 'Expr', 'std::unique_ptr<Expr>|callee, Token|oparn, std::unique_ptr<ArgList>|args'),
-    ASTClass('PrimaryExpr', 'Expr', 'Token|value'),
+    AST('CU',  'CUB', 'std::unique_ptr<DeclList>|decls'),
+    AST('DeclList',  'Decl', 'std::vector<std::unique_ptr<Decl>>|decls'),
+    AST('FunctionDecl', 'Decl', 'std::unique_ptr<Type>|retty, Token|name, std::unique_ptr<ParamList>|params, std::unique_ptr<Block>|body'),
+    AST('VarStmt', 'Stmt', 'std::unique_ptr<VarStmtItemList>|assignments'),
+    AST('VarStmtItem', 'VStmtIB', 'std::unique_ptr<Type>|type, Token|name, Token|equal, std::unique_ptr<Expr>|expr'),
+    AST('VarStmtItemList', 'VStmtIB', 'std::vector<std::unique_ptr<VarStmtItem>>|items'),
+    AST('ExprStmt', 'Stmt', 'std::unique_ptr<Expr>|expr'),
+    AST('RetStmt', 'Stmt', 'std::unique_ptr<Expr>|expr'),
+    AST('StmtList', 'Stmt', 'std::vector<std::unique_ptr<Stmt>>|stmts'),
+    AST('ImplRet', 'ImplRetB', 'std::unique_ptr<Expr>|expr'),
+    AST('PrimitiveType', 'Type', 'Token|ty'),
+    AST('PointerType', 'Type', 'std::unique_ptr<Type>|type'),
+    AST('Arg', 'ArgB', 'std::unique_ptr<Expr>|expr'),
+    AST('ArgList', 'ArgB', 'std::vector<std::unique_ptr<Arg>>|args'),
+    AST('Param', 'ParamB', 'std::unique_ptr<Type>|type, Token|name'),
+    AST('ParamList', 'ParamB', 'std::vector<std::unique_ptr<Param>>|params'),
+    AST('Block', 'Expr', 'std::unique_ptr<StmtList>|stmts, std::unique_ptr<ImplRet>|implRet'),
+    AST('IfExpr', 'Expr', 'Token|iftok, std::unique_ptr<Expr>|cond, std::unique_ptr<Expr>|trues, std::unique_ptr<Expr>|falses'),
+    AST('ForExpr', 'Expr', 'std::unique_ptr<VarStmt>|initial, std::unique_ptr<Expr>|cond, std::unique_ptr<Expr>|increment, std::unique_ptr<Expr>|body'),
+    AST('AssignmentExpr', 'Expr', 'std::unique_ptr<Expr>|target, Token|equal, std::unique_ptr<Expr>|expr'),
+    AST('ShortCircuitExpr', 'Expr', 'std::unique_ptr<Expr>|lhs, Token|op, std::unique_ptr<Expr>|rhs'),
+    AST('BinaryExpr', 'Expr', 'std::unique_ptr<Expr>|lhs, Token|op, std::unique_ptr<Expr>|rhs'),
+    AST('CastExpr', 'Expr', 'std::unique_ptr<Type>|type, std::unique_ptr<Expr>|expr'),
+    AST('UnaryExpr', 'Expr', 'Token|op, std::unique_ptr<Expr>|expr'),
+    AST('AddrofExpr', 'Expr', 'Token|op, std::unique_ptr<Expr>|expr'),
+    AST('DerefExpr', 'Expr', 'Token|op, std::unique_ptr<Expr>|expr'),
+    AST('CallExpr', 'Expr', 'std::unique_ptr<Expr>|callee, Token|oparn, std::unique_ptr<ArgList>|args'),
+    AST('PrimaryExpr', 'Expr', 'Token|value'),
 ]
 # Generating methods {{{1
 # Generating AST stuff {{{2
@@ -85,7 +85,7 @@ def gen_ast_decls():
         output.append(f'    class {ast.name};\n')
 
     for ast in asts:
-        if isinstance(ast, ASTClass):
+        if isinstance(ast, AST):
             output.append(f'    class {ast.name} : public {ast.base} {{\n')
 
             output.append('    public:\n')
@@ -100,7 +100,7 @@ def gen_ast_decls():
             output.append(f'        {ast.name}(File const &file, Location start, Location end, {", ".join(f"{field.type} {field.name}" for field in ast.fields)});\n')
 
             output.append('    };\n')
-        elif isinstance(ast, ASTBaseClass):
+        elif isinstance(ast, ASTBase):
             output.append(f'    class {ast.name} : public AST {{\n')
             output.append('    public:\n')
 
@@ -108,7 +108,7 @@ def gen_ast_decls():
             output.append('        public:\n')
             output.append('            virtual ~Visitor() {}\n')
             for _ast in asts:
-                if isinstance(_ast, ASTClass) and _ast.base == ast.name:
+                if isinstance(_ast, AST) and _ast.base == ast.name:
                     output.append(f'            virtual void visit{_ast.name}(ASTNS::{_ast.name} *ast) = 0;\n')
             output.append('        };\n')
 
@@ -131,7 +131,7 @@ def gen_ast_decls():
 def gen_ast_defs():
     output = ['#include "ast/ast.h"\n']
     for ast in asts:
-        if isinstance(ast, ASTClass):
+        if isinstance(ast, AST):
             output.append(f'ASTNS::{ast.name}::{ast.name}(File const &file, Location start, Location end, {", ".join(f"{field.type} {field.name}" for field in ast.fields)}): ')
 
             init_list = [f'{ast.base}(file), _start(start), _end(end)']
@@ -144,7 +144,7 @@ def gen_ast_defs():
             output.append(f'void ASTNS::{ast.name}::accept(ASTNS::{ast.base}::Visitor *v) {{ v->visit{ast.name}(this); }}\n')
             output.append(f'Location const & ASTNS::{ast.name}::start() {{ return _start; }}\n')
             output.append(f'Location const & ASTNS::{ast.name}::end() {{ return _end; }}\n')
-        elif isinstance(ast, ASTBaseClass):
+        elif isinstance(ast, ASTBase):
             output.append(f'ASTNS::{ast.name}::{ast.name}(File const &file): AST(file) {{}}\n')
         else:
             output.append('ASTNS::AST::AST(File const &file): file(file) {}\n')
@@ -155,7 +155,7 @@ def gen_ast_defs():
 def gen_visitor_methods(*bases):
     output = []
     for ast in asts:
-        if not isinstance(ast, ASTClass):
+        if not isinstance(ast, AST):
             continue
 
         if (ast.base in bases or bases == ('all',)):
@@ -164,13 +164,13 @@ def gen_visitor_methods(*bases):
     return''.join(output)
 # Generate inheriting classes {{{3
 def gen_visitor_inherit_all():
-    return ',\n'.join([f'public ASTNS::{cl.name}::Visitor' for cl in asts if isinstance(cl, ASTBaseClass)]) + '\n'
+    return ',\n'.join([f'public ASTNS::{cl.name}::Visitor' for cl in asts if isinstance(cl, ASTBase)]) + '\n'
 # Generating printing stuff {{{2
 # Genearte print visitor {{{3
 def gen_print_visitor_methods():
     output = []
     for ast in asts:
-        if not isinstance(ast, ASTClass):
+        if not isinstance(ast, AST):
             continue
 
         output.append(          f'void ASTNS::PrintVisitor::visit{ast.name}(ASTNS::{ast.name} *a) {{\n')
