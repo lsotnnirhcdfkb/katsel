61c7cdb8b2a168c2db3540464596d867a376de8e
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-01-27 10:19:03 -0500

    fix parser consuming

--- src/parse/parser.cpp
+++ src/parse/parser.cpp
@@ -5,29 +5,19 @@
 Parser::Parser(Lexer &l, File &sourcefile): lexer(l), sourcefile(sourcefile), errored(false) {}
 
 std::unique_ptr<ASTNS::CUB> Parser::parse() {
-    std::unique_ptr<ASTNS::CUB> ret (nullptr);
-
-    std::vector<stackitem> stack;
-    stack.emplace_back(0);
-
-    _parse(*this, stack, false, ret, consume());
-
-    if (errored)
-        return nullptr;
-
-    return ret;
+    return _parse(*this);
 }
 
-Token Parser::consume() {
+Located<TokenData> Parser::consume() {
     bool lastboom = false;
     while (true) {
-        Token cur (lexer.next_token());
-        if (cur.is<Tokens::Error>()) {
+        Located<TokenData> cur (lexer.next_token());
+        if (Tokens::is<Tokens::Error>(cur.value)) {
             errored = true;
-            (*cur.as<Tokens::Error>().errf)(cur);
-        } else if (cur.is<Tokens::Boom>()) {
+            (*Tokens::as<Tokens::Error>(cur.value).errf)(cur.span);
+        } else if (Tokens::is<Tokens::Boom>(cur.value)) {
             lastboom = true;
-        } else if (lastboom && cur.is<Tokens::Newline>()) {
+        } else if (lastboom && Tokens::is<Tokens::Newline>(cur.value)) {
             lastboom = false;
         } else {
             return cur;
