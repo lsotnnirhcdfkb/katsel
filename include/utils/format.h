14eb75687ef5b210db162990e6682e7babbe6465
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-01-26 22:44:08 -0500

    remove double underscores from format

--- include/utils/format.h
+++ include/utils/format.h
@@ -6,48 +6,48 @@
 
 #include "message/report_abort.h"
 
-namespace __fmt {
-    struct __FmtAction {
+namespace _fmt {
+    struct _FmtAction {
         enum class Type { INSERT_OPEN_BRACE, INSERT_CLOSE_BRACE, INSERT_ARG, INSERT_NEXT_CHAR } type;
         int di;
     };
 
-    __FmtAction __decide_action(std::string::const_iterator i, std::string::const_iterator end);
-    void __format(std::stringstream &ss, std::string const &fstr, std::string::const_iterator start);
+    _FmtAction _decide_action(std::string::const_iterator i, std::string::const_iterator end);
+    void _format(std::stringstream &ss, std::string const &fstr, std::string::const_iterator start);
 
     template <typename First, typename ... Formats>
-    void __format(std::stringstream &ss, std::string const &fstr, std::string::const_iterator start, First &&first, Formats && ...f) {
+    void _format(std::stringstream &ss, std::string const &fstr, std::string::const_iterator start, First &&first, Formats && ...f) {
         for (auto i = start; i != fstr.cend(); ) {
-            __FmtAction a (__decide_action(i, fstr.cend()));
+            _FmtAction a (_decide_action(i, fstr.cend()));
             switch (a.type) {
-                case __FmtAction::Type::INSERT_OPEN_BRACE:
+                case _FmtAction::Type::INSERT_OPEN_BRACE:
                     ss << '{';
                     break;
 
-                case __FmtAction::Type::INSERT_CLOSE_BRACE:
+                case _FmtAction::Type::INSERT_CLOSE_BRACE:
                     ss << '}';
                     break;
 
-                case __FmtAction::Type::INSERT_NEXT_CHAR:
+                case _FmtAction::Type::INSERT_NEXT_CHAR:
                     ss << *i;
                     break;
 
-                case __FmtAction::Type::INSERT_ARG:
+                case _FmtAction::Type::INSERT_ARG:
                     ss << std::forward<First>(first);
-                    __format(ss, fstr, i + a.di, std::forward<Formats>(f)...);
+                    _format(ss, fstr, i + a.di, std::forward<Formats>(f)...);
                     return;
             }
 
             i += a.di;
         }
 
-        report_abort_noh("unused interpolation arguments in __format call");
+        report_abort_noh("unused interpolation arguments in _format call");
     }
 }
 
 template <typename ... Formats>
 std::string format(std::string const &fstr, Formats && ...f) {
     std::stringstream ss;
-    __fmt::__format(ss, fstr, fstr.cbegin(), std::forward<Formats>(f)...);
+    _fmt::_format(ss, fstr, fstr.cbegin(), std::forward<Formats>(f)...);
     return ss.str();
 }
--- src/utils/format.cpp
+++ src/utils/format.cpp
@@ -1,44 +1,44 @@
 #include "utils/format.h"
 
-__fmt::__FmtAction __fmt::__decide_action(std::string::const_iterator i, std::string::const_iterator end) {
+_fmt::_FmtAction _fmt::_decide_action(std::string::const_iterator i, std::string::const_iterator end) {
     char next = (i + 1) == end ? '\0' : *(i + 1);
     switch (*i) {
         case '{':
             switch (next) {
-                case '{': return __FmtAction { __FmtAction::Type::INSERT_OPEN_BRACE, 2 };
-                case '}': return __FmtAction { __FmtAction::Type::INSERT_ARG, 2 };
+                case '{': return _FmtAction { _FmtAction::Type::INSERT_OPEN_BRACE, 2 };
+                case '}': return _FmtAction { _FmtAction::Type::INSERT_ARG, 2 };
                 default : report_abort_noh("expected '}' in format string");
             }
 
         case '}':
             switch (next) {
-                case '}': return __FmtAction { __FmtAction::Type::INSERT_CLOSE_BRACE, 2 };
+                case '}': return _FmtAction { _FmtAction::Type::INSERT_CLOSE_BRACE, 2 };
                 default: report_abort_noh("unexpected '}' in format string");
             }
 
         default:
-            return __FmtAction { __FmtAction::Type::INSERT_NEXT_CHAR, 1 };
+            return _FmtAction { _FmtAction::Type::INSERT_NEXT_CHAR, 1 };
     }
 }
 
-void __fmt::__format(std::stringstream &ss, std::string const &fstr, std::string::const_iterator start) {
+void _fmt::_format(std::stringstream &ss, std::string const &fstr, std::string::const_iterator start) {
     for (auto i = start; i != fstr.cend(); ) {
-        __FmtAction a (__decide_action(i, fstr.cend()));
+        _FmtAction a (_decide_action(i, fstr.cend()));
         switch (a.type) {
-            case __FmtAction::Type::INSERT_OPEN_BRACE:
+            case _FmtAction::Type::INSERT_OPEN_BRACE:
                 ss << '{';
                 break;
 
-            case __FmtAction::Type::INSERT_CLOSE_BRACE:
+            case _FmtAction::Type::INSERT_CLOSE_BRACE:
                 ss << '}';
                 break;
 
-            case __FmtAction::Type::INSERT_NEXT_CHAR:
+            case _FmtAction::Type::INSERT_NEXT_CHAR:
                 ss << *i;
                 break;
 
-            case __FmtAction::Type::INSERT_ARG:
-                report_abort_noh("no more arguments to interpolate in __format call");
+            case _FmtAction::Type::INSERT_ARG:
+                report_abort_noh("no more arguments to interpolate in _format call");
         }
 
         i += a.di;
