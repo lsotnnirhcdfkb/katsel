bd4735619297a6f5cdfe84bf5215d3dbc4301d6f
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-01-28 20:14:07 -0500

    add explicit token template instantiation to reduce compile times hopefully

--- src/lex/token.cpp
+++ src/lex/token.cpp
@@ -1,6 +1,8 @@
 #include "lex/token.h"
 #include "lex/tokens.h"
 
+template class TOKEN_VARIANT_TYPE;
+
 std::string Tokens::stringify_type(TokenData const &td) {
     return std::visit([] (auto &&arg) -> std::string {
             using T = std::decay_t<decltype(arg)>;
