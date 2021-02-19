9d2ca86282f07d7424152d3c3bf668f46e1706a7
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-02-19 08:52:10 -0500

    fix result class

--- include/utils/result.h
+++ include/utils/result.h
@@ -7,31 +7,46 @@
 template <typename T, typename E>
 class Result {
 public:
-    template <typename U, typename = std::enable_if_t<std::is_constructible_v<T, U>>
-    Result(U &&thing): Data( success_t { std::forward<U>(thing) } );
+    template <typename U, typename = std::enable_if_t<std::is_convertible_v<U, T>>>
+    static Result<T, E> Success(U &&thing) {
+        return Result<T, E> { std::forward<U>(thing) };
+    }
 
-    template <typename F, typename = std::enable_if_t<std::is_constructible_v<E, F>>
-    Result(F &&err): Data( errored_t { std::forward<F>(err) } );
+    template <typename F, typename = std::enable_if_t<std::is_convertible_v<F, E>>>
+    static Result<T, E> Error(F &&thing) {
+        return Result<T, E> { std::forward<F>(thing) };
+    }
+
+    template <typename DataType, typename ErrType,
+             typename = std::enable_if_t<std::is_convertible_v<DataType, T>>,
+             typename = std::enable_if_t<std::is_convertible_v<ErrType, E>>>
+    Result(Result<DataType, ErrType> &&thing):
+        data {
+            thing.success() ?
+            thing.get_val() :
+            thing.get_err()
+        } {}
+                 
 
     template <typename SuccessOp, typename ErrOp, typename Ret = std::invoke_result_t<SuccessOp, T const &>>
     inline Ret match(SuccessOp s, ErrOp e) const {
         if (success()) {
-            return s(get_val());
+            return s(result());
         } else {
-            return e(get_err());
+            return e(error());
         }
     }
+
+    bool success() const {
+        return std::holds_alternative<success_t>(data);
+    }
+
+    T const &result() const { ASSERT( success()); return std::get<success_t>(data).value; }
+    E const &error() const { ASSERT(!success()); return std::get<errored_t>(data).err  ; }
 
 private:
     struct success_t { T value; };
     struct errored_t { E err; };
 
     std::variant<success_t, errored_t> data;
-
-    bool success() const {
-        return std::holds_alternative<success_t>(data);
-    }
-
-    T const &get_val() const { ASSERT( success()); return std::get<success_t>(data).value; }
-    E const &get_err() const { ASSERT(!success()); return std::get<Erorred>(data).err  ; }
-}
+};
