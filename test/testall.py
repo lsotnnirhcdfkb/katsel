3561ffef97120f8201a6f04949edd2e9c984b976
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-02-20 22:36:35 -0500

    if -> elif

--- test/testall.py
+++ test/testall.py
@@ -213,7 +213,7 @@ except KeyboardInterrupt:
 else:
     if num_failed == 0:
         write('\033[32;1mALL TESTS PASSED!\033[0m\n')
-    if num_passed == 0:
+    elif num_passed == 0:
         write('\033[31;1mBAD NEWS: ALL TESTS FAILED!\033[0m\n')
         sys.exit(1)
     else:
