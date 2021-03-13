ebada4c76581492a0958df5187b18d214a60a5f5
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-03-12 20:13:44 -0500

    remove old files

--- include/codegen/codegen.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace ASTNS { class CUB; }
-#include "ir/unit.h"
-#include "utils/ptr.h"
-
-namespace Codegen {
-    Maybe<IR::Unit> codegen(File const &file, NNPtr<ASTNS::CUB> cub);
-}
--- include/message/report_abort.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-void report_abort_noh [[ noreturn ]] (std::string const &message);
