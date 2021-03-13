f1c2cd6d087baa274db2f747519710322471b56f
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-03-12 23:56:01 -0500

    add helper functions

--- src/Lex.hs
+++ src/Lex.hs
@@ -84,7 +84,7 @@ data Token = OParen
            | Dedent
            | Newline
            | EOF
-           | Error String -- TODO: error reporting
+           | Error String
            deriving Show
 
 data Lexer = Lexer
@@ -107,13 +107,42 @@ lex f = lex' $ Lexer
 lex' :: Lexer -> [Located Token]
 lex' lexer =
     case remaining lexer of
-        _ -> [makeToken lexer 1 $ Error "No"]
-
-makeToken :: Lexer -> Int -> Token -> Located Token
-makeToken lexer len tok =
-    Located (makeSpan file srci l c len) tok
+        [] ->
+            [fst $ makeToken 1 EOF]
+        _ ->
+            continueLex $ makeToken 1 $ Error "No"
     where
-        file = sourcefile lexer
-        srci = sourceLocation lexer
-        l = lnn lexer
-        c = coln lexer
+        makeToken len tok = (makeToken' len tok, lexer `advance` len)
+
+        continueLex (tok1, nextLexer) =
+            tok1 : lex' nextLexer
+
+        makeToken' len tok =
+            Located (makeSpan file srci l c len) tok
+            where
+                file = sourcefile lexer
+                srci = sourceLocation lexer
+                l = lnn lexer
+                c = coln lexer
+
+advance :: Lexer -> Int -> Lexer
+advance lexer 0 = lexer
+advance lexer 1 = lexer
+                  { sourceLocation = sourceLocation lexer + 1
+                  , remaining = drop 1 $ remaining lexer
+                  , lnn = nextlnn
+                  , coln = nextcoln
+                  }
+    where
+        overNL = case remaining lexer of
+            '\n':_ -> True
+            _ -> False
+
+        nextlnn = if overNL
+            then lnn lexer + 1
+            else lnn lexer
+        nextcoln = if overNL
+            then 1
+            else coln lexer + 1
+
+advance lexer n = advance (advance lexer 1) $ n - 1
