4b0d2106078ecce81c7068005eac0d37a5a7f558
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-03-14 17:28:36 -0400

    add type alias

--- src/Driver.hs
+++ src/Driver.hs
@@ -14,13 +14,15 @@ data Backend = CBackend
 
 data OutputFormat = Lexed | Parsed | KatselIR | BackendCode Backend
 
-joinStages :: (a -> (b, [Message.SimpleDiag])) -> (b -> (c, [Message.SimpleDiag])) -> (a -> (c, [Message.SimpleDiag]))
+type Stage a b = (a -> (b, [Message.SimpleDiag]))
+
+joinStages :: Stage a b -> Stage b c -> Stage a c
 joinStages stage1fn stage2fn = \ input ->
     let (stage1res, stage1errs) = stage1fn input
         (stage2res, stage2errs) = stage2fn stage1res
     in (stage2res, stage1errs ++ stage2errs)
 
-lexStage :: File -> ([Located Lex.Token], [Message.SimpleDiag])
+lexStage :: Stage File [Located Lex.Token]
 lexStage contents =
     let lexed = Lex.lex contents
     in ([x | Right x <- lexed], [Message.toDiagnostic x | Left x <- lexed])
