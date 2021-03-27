5c597a07846d01f1ad39a8d34b790fbc434b546c
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-03-27 14:18:28 -0400

    add CG data types

--- src/LowerAST.hs
+++ src/LowerAST.hs
@@ -3,5 +3,35 @@ module LowerAST(lowerCU) where
 import qualified IR
 import qualified AST
 
+import Location
+
+data CGT0
+    = FunctionCGT0
+    | ImplCGT0 AST.LDType [CGT0]
+
+data CGT1
+    = FunctionCGT1
+    | ImplCGT1
+
+data CGV0
+    = FunctionCGV0
+    | ImplCGV0
+
+data CGV1
+    = FunctionCGV1
+    | ImplCGV1
+
 lowerCU :: AST.LDCU -> Maybe IR.Unit
-lowerCU _ = error "TODO"
+lowerCU (Located _ (AST.DCU'CU decls)) = error "TODO"
+    where
+        declcgs = map declCG decls
+
+declCG :: AST.LDDecl -> CGT0
+declCG (Located _ (AST.DDecl'Fun sf)) = funCG sf
+declCG (Located _ (AST.DDecl'Impl implFor members)) = ImplCGT0 implFor $ map implMemberCG members
+
+funCG :: AST.LSFunDecl -> CGT0
+funCG (Located _ (AST.SFunDecl' _ _ _ _)) = FunctionCGT0
+
+implMemberCG :: AST.LDImplMember -> CGT0
+implMemberCG (Located _ (AST.DImplMember'Fun sf)) = funCG sf
