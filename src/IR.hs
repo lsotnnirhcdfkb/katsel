a6d18cf771a8f15daf79dc8da53351a4c970ccc7
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-03-27 09:19:45 -0400

    declsymbol children

--- src/IR.hs
+++ src/IR.hs
@@ -4,11 +4,37 @@ module IR
 
 import File
 
+import Data.Map(Map)
+import qualified Data.Map as Map
+
+type StrMap = Map String
+
 data Unit = Unit File Module
 
-data Module = Module
+data Module = Module (StrMap DeclSymbol)
 
-data Type = Type
+data Type = Type (StrMap DeclSymbol)
 
-data DeclSymbol = DSModule Module
-                | DSType Type
+data DeclSymbol
+    = DSModule Module
+    | DSType Type
+
+data Value
+
+getValues :: DeclSymbol -> StrMap ()
+getValues (DSModule (Module map)) = error "values not implemented yet"
+getValues (DSType (Type map)) = error "values not implemented yet"
+
+getDeclSymbols :: DeclSymbol -> StrMap DeclSymbol
+getDeclSymbols (DSModule (Module dsmap)) = dsmap
+getDeclSymbols (DSType (Type dsmap)) = dsmap
+
+getValue :: DeclSymbol -> String -> Maybe ()
+getValue ds n = Map.lookup n $ getValues ds
+
+getDeclSymbol :: DeclSymbol -> String -> Maybe DeclSymbol
+getDeclSymbol ds n = Map.lookup n $ getDeclSymbols ds
+
+-- TODO:
+-- addValue =
+-- addDeclSymbol =
