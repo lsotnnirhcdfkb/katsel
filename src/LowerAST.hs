9b2b70f761b8db89f5665d096cd267e89a94b177
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-03-28 14:58:00 -0400

    remove all CG things, replace with functions

--- src/LowerAST.hs
+++ src/LowerAST.hs
@@ -5,52 +5,12 @@ import qualified AST
 
 import Location
 
--- each cg represents the data needed in order to do the phase
--- so DDeclCG represents the data needed to do the declaration declaration phase
--- and VDefCG represents the data needed to do the value definition phase
-
-data DDeclCG
-    = ModuleDDeclCG [DDeclCG]
-    | FunctionDDeclCG DDeclCG AST.LSFunDecl
-    | ImplDDeclCG AST.LDType [DDeclCG]
-
-data DDefCG
-    = ModuleDDefCG
-    | FunctionDDefCG
-    | ImplDDefCG
-
-data VDeclCG
-    = ModuleVDeclCG
-    | FunctionVDeclCG
-    | ImplVDeclCG
-
-data VDefCG
-    = ModuleVDefCG
-    | FunctionVDefCG
-    | ImplVDefCG
-
 lowerMod :: AST.LDModule -> Maybe IR.Unit
 lowerMod (Located _ (AST.DModule' decls)) = error "TODO"
-    where
-        modcg = ModuleDDeclCG $ map (declCG modcg) decls
 
-declCG :: DDeclCG -> AST.LDDecl -> DDeclCG
-declCG parent (Located _ (AST.DDecl'Fun sf)) = FunctionDDeclCG parent sf
-declCG _ (Located _ (AST.DDecl'Impl implFor members)) = parentCG
-    where
-        parentCG = ImplDDeclCG implFor $ map (implMemberCG parentCG) members
+ddeclare, ddefine, vdeclare, vdefine :: IR.Unit -> AST.DDecl -> IR.Unit
 
-implMemberCG :: DDeclCG -> AST.LDImplMember -> DDeclCG
-implMemberCG parent (Located _ (AST.DImplMember'Fun sf)) = FunctionDDeclCG parent sf
-
-ddecl :: DDeclCG -> DDefCG
-ddecl = undefined
-
-ddef :: DDefCG -> VDeclCG
-ddef = undefined
-
-vdecl :: VDeclCG -> VDefCG
-vdecl = undefined
-
-vdef :: VDefCG -> ()
-vdef = undefined
+ddeclare = undefined
+ddefine = undefined
+vdeclare = undefined
+vdefine = undefined
