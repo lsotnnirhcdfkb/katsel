c447d8fa48c495bb04cf0a0d3feedf4069d9e32f
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-03-30 17:51:24 -0400

    more extensions! add get function into Parent class

--- src/LowerAST.hs
+++ src/LowerAST.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module LowerAST(lowerMod) where
 
@@ -26,33 +28,43 @@ lowerMod lmod =
 
 class Lowerable l p where
     ddeclare, ddefine, vdeclare, vdefine :: p -> l -> p
-class Parent p c where
-    add :: p -> c -> p
+class Parent p c i | p c -> i where
+    add :: p -> i -> c -> p
+    get :: p -> i -> Maybe c
 
 type ModParent = Maybe IR.Module
-instance Parent ModParent IR.Module where
-    add _ m = Just m
+instance Parent ModParent IR.Module () where
+    add _ _ m = Just m
+    get = undefined
 
-instance Parent IR.DeclSymbol IR.DeclSymbol where
+instance Parent IR.DeclSymbol IR.DeclSymbol String where
     add = undefined
-instance Parent IR.DeclSymbol IR.Value where
+    get = undefined
+instance Parent IR.DeclSymbol IR.Value String where
     add = undefined
-instance Parent IR.Module IR.Value where
+    get = undefined
+instance Parent IR.Module IR.Value String where
     add = undefined
-instance Parent IR.Module IR.DeclSymbol where
+    get = undefined
+instance Parent IR.Module IR.DeclSymbol String where
     add = undefined
+    get = undefined
 
-instance Parent p IR.Module => Lowerable AST.LDModule p where
-    ddeclare parent (Located _ (AST.DModule' decls)) = add parent finalModule
+instance Parent p IR.Module () => Lowerable AST.LDModule p where
+    ddeclare parent (Located _ (AST.DModule' decls)) = add parent () finalModule
         where
             startModule = IR.Module Map.empty Map.empty
             finalModule = foldl' ddeclare startModule decls
 
-    ddefine = undefined
+    ddefine parent (Located _ (AST.DModule' decls)) = add parent () defined
+        where
+            (Just parentmod) = get parent () :: Maybe IR.Module -- not sure why this type annotation is needed to compile
+            defined = foldl' ddefine parentmod decls
+
     vdeclare = undefined
     vdefine = undefined
 
-instance Parent p IR.Value => Lowerable AST.LDDecl p where
+instance Parent p IR.Value String => Lowerable AST.LDDecl p where
     ddeclare _ (Located _ (AST.DDecl'Fun _)) = undefined
     ddeclare _ (Located _ (AST.DDecl'Impl _ _)) = undefined
 
