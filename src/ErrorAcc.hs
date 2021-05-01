f4ef22ef2694320a4ef83d5baf7d9e1c9f11ab57
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-05-01 10:41:08 -0400

    add missing file

---
+++ src/ErrorAcc.hs
@@ -0,0 +1,24 @@
+module ErrorAcc where
+
+import qualified Message
+
+data ErrorAccumulated r = ErrorAcc r [Message.SimpleDiag]
+
+instance Functor ErrorAccumulated where
+    fmap f (ErrorAcc v errs) = ErrorAcc (f v) errs
+
+instance Applicative ErrorAccumulated where
+    pure x = ErrorAcc x []
+
+    eaf <*> eav =
+        eaf >>= \ f ->
+        eav >>= \ v ->
+        pure $ f v
+
+instance Monad ErrorAccumulated where
+    (ErrorAcc aval aerrs) >>= f =
+        let (ErrorAcc bval berrs) = f aval
+        in ErrorAcc bval $ aerrs ++ berrs
+
+addErrors :: [Message.SimpleDiag] -> ErrorAccumulated ()
+addErrors = ErrorAcc ()
