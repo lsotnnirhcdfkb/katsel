307beed1d76499884abf7311719c7581d8d1ea26
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-05-03 18:40:57 -0400

    Merge DSChildren and VChildren with Parent
    instances of 'DSChildren d' become 'Parent d DeclSymbol String'
    instances of 'VChildren d' become 'Parent d Value String'

--- src/IR.hs
+++ src/IR.hs
@@ -27,8 +27,7 @@ import Control.Monad.State.Lazy(state, execState)
 
 {-
 TODO: more typeclasses!
-    split DSChildren and VChildren into DSChildGet, DSChildAdd, VChildGet, and VChildAdd
-    and also split Parent into ParentWriteOnly and ParentReadOnly
+    split Parent into ParentWrite and ParentRead
     in order to make sure that lowering functions only read from the first arguments and only modify in the IRDiff
 -}
 
@@ -54,7 +53,7 @@ dsresolve :: Typeable r => Module -> DSIRId r -> Maybe r
 dsresolve parentmod (DSIRId path) =
     foldl' next (Just $ DeclSymbol parentmod) path >>= cast
     where
-        next (Just ds) name = get_decl_symbol name ds
+        next (Just ds) name = get name ds :: Maybe DeclSymbol
         next Nothing _ = Nothing
 
 vresolve :: Typeable r => Module -> VIRId r -> Maybe r
@@ -62,11 +61,18 @@ vresolve parentmod (VIRId parent childname) =
     child >>= cast
     where
         parent_resolved = dsresolve parentmod parent
-        child = parent_resolved >>= get_value childname
+        child = parent_resolved >>= get childname :: Maybe Value
 -- IR datatypes {{{1
+-- Parent class {{{2
+class Ord i => Parent p c i | p c -> i where
+    get_child_map :: p -> Map i c
+    get :: i -> p -> Maybe c
+    add :: i -> c -> p -> p
+
+    get ind parent = Map.lookup ind (get_child_map parent)
 -- DeclSymbols {{{2
 data DeclSymbol where
-    DeclSymbol :: (Typeable d, DSChildren d, VChildren d) => d -> DeclSymbol
+    DeclSymbol :: (Typeable d, Parent d DeclSymbol String, Parent d Value String) => d -> DeclSymbol
 
 data Module = Module DSMap VMap
 
@@ -79,42 +85,28 @@ data Type
     | FunctionType DSMap TyIdx [(Mutability, TyIdx)]
     | VoidType DSMap
     | PointerType DSMap Mutability TyIdx
--- classes {{{3
-class DSChildren d where
-    get_dsmap :: d -> DSMap
-    get_decl_symbol :: String -> d -> Maybe DeclSymbol
-    add_decl_symbol :: String -> DeclSymbol -> d -> d
-
-    get_decl_symbol n d = Map.lookup n $ get_dsmap d
-
-class VChildren v where
-    get_vmap :: v -> VMap
-    get_value :: String -> v -> Maybe Value
-    add_value :: String -> Value -> v -> v
-
-    get_value n v = Map.lookup n $ get_vmap v
 -- instances {{{3
--- DeclSymbol {{{3
-instance DSChildren DeclSymbol where
-    get_dsmap (DeclSymbol d) = get_dsmap d
-    add_decl_symbol name child (DeclSymbol ds) = DeclSymbol $ add_decl_symbol name child ds
-instance VChildren DeclSymbol where
-    get_vmap (DeclSymbol d) = get_vmap d
-    add_value name child (DeclSymbol ds) = DeclSymbol $ add_value name child ds
+-- DeclSymbol {{{4
+instance Parent DeclSymbol DeclSymbol String where
+    get_child_map (DeclSymbol d) = get_child_map d
+    add name child (DeclSymbol ds) = DeclSymbol $ add name child ds
+instance Parent DeclSymbol Value String where
+    get_child_map (DeclSymbol d) = get_child_map d
+    add name child (DeclSymbol ds) = DeclSymbol $ add name child ds
 -- Type {{{4
-instance DSChildren Type where
-    get_dsmap (FloatType dsmap _) = dsmap
-    get_dsmap (IntType dsmap _ _) = dsmap
-    get_dsmap (CharType dsmap) = dsmap
-    get_dsmap (BoolType dsmap) = dsmap
-    get_dsmap (FunctionType dsmap _ _) = dsmap
-    get_dsmap (VoidType dsmap) = dsmap
-    get_dsmap (PointerType dsmap _ _) = dsmap
+instance Parent Type DeclSymbol String where
+    get_child_map (FloatType dsmap _) = dsmap
+    get_child_map (IntType dsmap _ _) = dsmap
+    get_child_map (CharType dsmap) = dsmap
+    get_child_map (BoolType dsmap) = dsmap
+    get_child_map (FunctionType dsmap _ _) = dsmap
+    get_child_map (VoidType dsmap) = dsmap
+    get_child_map (PointerType dsmap _ _) = dsmap
 -- Module {{{4
-instance DSChildren Module where
-    get_dsmap (Module dsmap _) = dsmap
-instance VChildren Module where
-    get_vmap (Module _ vmap) = vmap
+instance Parent Module DeclSymbol String where
+    get_child_map (Module dsmap _) = dsmap
+instance Parent Module Value String where
+    get_child_map (Module _ vmap) = vmap
 -- Values {{{2
 data Value where
     Value :: (Typeable v) => v -> Value
@@ -214,29 +206,12 @@ get_void_type = error "not implemented yet"
 newtype IRDiff p = IRDiff { apply_diff :: (p, IRBuilder) -> (p, IRBuilder) }
 class Lowerable l p where
     ddeclare, ddefine, vdeclare, vdefine :: l -> p -> Module -> IRDiff p
--- Parent class {{{2
-class Parent p c i | p c -> i where
-    add :: i -> c -> p -> p
-    get :: i -> p -> Maybe c
 -- lowering modules {{{2
 newtype ModParent = ModParent Module
--- parent instances {{{
 instance Parent ModParent Module () where
+    get_child_map (ModParent mp) = Map.fromList [((), mp)]
     add _ m _ = ModParent m
-    get _ (ModParent m) = Just m
-instance Parent DeclSymbol DeclSymbol String where
-    add = add_decl_symbol
-    get = get_decl_symbol
-instance Parent DeclSymbol Value String where
-    add = add_value
-    get = get_value
-instance Parent Module DeclSymbol String where
-    add = add_decl_symbol
-    get = get_decl_symbol
-instance Parent Module Value String where
-    add = add_value
-    get = get_value
--- }}}
+
 instance Parent p Module () => Lowerable AST.LDModule p where
     ddeclare (Located _ (AST.DModule' decls)) parent root =
         let (Just module_) = get () parent :: Maybe Module
