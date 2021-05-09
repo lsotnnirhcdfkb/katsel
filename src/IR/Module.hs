342906526849fdacf76357c4c7c0a0f1dae619c8
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-05-08 20:18:10 -0400

    implement add for Module

--- src/IR/Module.hs
+++ src/IR/Module.hs
@@ -12,6 +12,8 @@ import IR.Value
 import IR.DeclSpan
 import IR.Describe
 
+import qualified Data.Map as Map
+
 import Location
 
 data Module = Module DSMap VMap Span
@@ -22,7 +24,15 @@ instance ParentR Module Value String where
     get_child_map (Module _ vmap _) = vmap
 
 instance ParentW Module DeclSymbol String where
+    add name ds (Module dsmap vmap sp) =
+        let old_val = Map.lookup name dsmap
+            dsmap' = Map.insert name ds dsmap
+        in (old_val, Module dsmap' vmap sp)
 instance ParentW Module Value String where
+    add name ds (Module dsmap vmap sp) =
+        let old_val = Map.lookup name vmap
+            vmap' = Map.insert name ds vmap
+        in (old_val, Module dsmap vmap' sp)
 
 instance DeclSpan Module where
     decl_span (Module _ _ sp) = Just sp
