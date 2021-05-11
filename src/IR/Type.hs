8429467a9a349c922aa2a630b49937ca90c14362
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-05-11 14:27:24 -0400

    move ty_eq to TypeInterner

--- src/IR/Type.hs
+++ src/IR/Type.hs
@@ -6,7 +6,6 @@ module IR.Type
     , Signedness(..)
     , TyIdx
     , Type(..)
-    , ty_eq
     ) where
 
 import IR.MapSynonyms
@@ -25,26 +24,6 @@ data Type
     | FunctionType DSMap TyIdx [(Mutability, TyIdx)]
     | VoidType DSMap
     | PointerType DSMap Mutability TyIdx
-
-ty_eq :: Type -> Type -> Bool
-
-ty_eq (FloatType _ size_a) (FloatType _ size_b)
-    | size_a == size_b = True
-
-ty_eq (IntType _ size_a sign_a) (IntType _ size_b sign_b)
-    | size_a == size_b && sign_a == sign_b = True
-
-ty_eq (FunctionType _ ret_a params_a) (FunctionType _ ret_b params_b)
-    | ret_a == ret_b && params_a == params_b = True
-
-ty_eq (PointerType _ muty_a pointee_a) (PointerType _ muty_b pointee_b)
-    | muty_a == muty_b && pointee_a == pointee_b = True
-
-ty_eq (CharType _) (CharType _) = True
-ty_eq (BoolType _) (BoolType _) = True
-ty_eq (VoidType _) (VoidType _) = True
-
-ty_eq _ _ = False
 
 instance ParentR Type DeclSymbol String where
     get_child_map (FloatType dsmap _) = dsmap
--- src/IR/TypeInterner.hs
+++ src/IR/TypeInterner.hs
@@ -19,3 +19,23 @@ get_ty ty ctx@(TypeInterner tys) =
     case findIndex (ty_eq ty) tys of
         Just idx -> (TyIdx idx, ctx)
         Nothing -> (TyIdx $ length tys, TypeInterner $ tys ++ [ty])
+
+ty_eq :: Type -> Type -> Bool
+
+ty_eq (FloatType _ size_a) (FloatType _ size_b)
+    | size_a == size_b = True
+
+ty_eq (IntType _ size_a sign_a) (IntType _ size_b sign_b)
+    | size_a == size_b && sign_a == sign_b = True
+
+ty_eq (FunctionType _ ret_a params_a) (FunctionType _ ret_b params_b)
+    | ret_a == ret_b && params_a == params_b = True
+
+ty_eq (PointerType _ muty_a pointee_a) (PointerType _ muty_b pointee_b)
+    | muty_a == muty_b && pointee_a == pointee_b = True
+
+ty_eq (CharType _) (CharType _) = True
+ty_eq (BoolType _) (BoolType _) = True
+ty_eq (VoidType _) (VoidType _) = True
+
+ty_eq _ _ = False
