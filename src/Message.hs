d0365b8d3ae73c5ed925ca6440dc0799bdb22991
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-05-14 20:18:44 -0400

    fix spacing

--- src/Message.hs
+++ src/Message.hs
@@ -74,8 +74,8 @@ report' (SimpleDiag ty maybe_span maybe_diag_code maybe_name sections) =
         footer =
             case (maybe_diag_code, maybe_name) of
                 (Just (DiagCode diag_code), Just diag_name) -> prefix ++ surround '[' ']' (add_sgr Colors.diagcode_sgr diag_code) ++ ": " ++ add_sgr Colors.diagname_sgr diag_name ++ "\n"
-                (Nothing                 , Just diag_name) -> prefix ++ add_sgr Colors.diagname_sgr diag_name ++ "\n"
-                (Just (DiagCode diag_code), Nothing      ) -> prefix ++ surround '[' ']' (add_sgr Colors.diagcode_sgr diag_code) ++ "\n"
+                (Nothing                  , Just diag_name) -> prefix ++ add_sgr Colors.diagname_sgr diag_name ++ "\n"
+                (Just (DiagCode diag_code), Nothing       ) -> prefix ++ surround '[' ']' (add_sgr Colors.diagcode_sgr diag_code) ++ "\n"
 
                 _ -> ""
             where
