59463fa97ff24422cccafb542dcba5a9f4075251
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-05-30 23:13:48 -0400

    change a function

--- src/Message/Utils.hs
+++ src/Message/Utils.hs
@@ -1,7 +1,7 @@
 module Message.Utils where
 
 make_indent_with_divider :: Char -> String -> Int -> String
-make_indent_with_divider divider left indent = replicate (indent - length left - 1) ' ' ++ left ++ " " ++ [divider] ++ " "
+make_indent_with_divider divider left indent = make_indent_str (indent - length left - 1) ++ left ++ " " ++ [divider] ++ " "
 
 make_indent_str :: Int -> String
 make_indent_str x = replicate x ' '
