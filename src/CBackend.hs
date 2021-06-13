43b4241ba0c87f9d2671415bfe978fda8c7554b3
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-06-12 23:01:11 -0400

    add mangled path to argument to lowering functions

--- src/CBackend.hs
+++ src/CBackend.hs
@@ -9,7 +9,9 @@ lower_mod_to_c :: IR.IRCtx -> IR.Module -> String
 lower_mod_to_c irctx root =
     let (dses, vals) = IR.all_entities_in_mod irctx root
 
-        desc_wrapper action thing_type fun mangle_fun (path, thing) = "// " ++ action ++ " of " ++ thing_type ++ " '" ++ show path ++ "' mangled as '" ++ Mangle.mangled_str (mangle_fun path) ++ "':\n" ++ fun irctx path thing ++ "\n"
+        desc_wrapper action thing_type fun mangle_fun (path, thing) =
+            let mangled = mangle_fun path
+            in "// " ++ action ++ " of " ++ thing_type ++ " '" ++ show path ++ "' mangled as '" ++ Mangle.mangled_str mangled ++ "':\n" ++ fun irctx path mangled thing ++ "\n"
         section action thing_type fun mangle_fun child_list =
             "// " ++ action ++ "s of " ++ thing_type ++ "s\n\n" ++
             concatMap (desc_wrapper action thing_type fun mangle_fun) child_list
@@ -21,32 +23,32 @@ lower_mod_to_c irctx root =
         ]
 
 -- decl_ds {{{1
-decl_ds :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> IR.DeclSymbol -> String
-decl_ds irctx name = IR.apply_to_ds (error "cannot declare module in c backend") (decl_tyidx irctx name)
+decl_ds :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> Mangle.MangledName -> IR.DeclSymbol -> String
+decl_ds irctx path mname = IR.apply_to_ds (error "cannot declare module in c backend") (decl_tyidx irctx path mname)
 
-decl_tyidx :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> IR.TyIdx -> String
-decl_tyidx irctx name = IR.apply_to_tyidx (decl_ty irctx name) irctx
+decl_tyidx :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> Mangle.MangledName -> IR.TyIdx -> String
+decl_tyidx irctx path mname = IR.apply_to_tyidx (decl_ty irctx path mname) irctx
 
-decl_ty :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> IR.Type -> String
-decl_ty _ _ _ = "#error declaration of type currently unsupported\n"
+decl_ty :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> Mangle.MangledName -> IR.Type -> String
+decl_ty _ _ _ _ = "#error declaration of type currently unsupported\n"
 -- def_ds {{{1
-def_ds :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> IR.DeclSymbol -> String
-def_ds irctx name = IR.apply_to_ds (error "cannot define module in c backend") (def_tyidx irctx name)
+def_ds :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> Mangle.MangledName -> IR.DeclSymbol -> String
+def_ds irctx path mname = IR.apply_to_ds (error "cannot define module in c backend") (def_tyidx irctx path mname)
 
-def_tyidx :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> IR.TyIdx -> String
-def_tyidx irctx name = IR.apply_to_tyidx (def_ty irctx name) irctx
+def_tyidx :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> Mangle.MangledName -> IR.TyIdx -> String
+def_tyidx irctx path mname = IR.apply_to_tyidx (def_ty irctx path mname) irctx
 
-def_ty :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> IR.Type -> String
-def_ty _ _ _ = "#error definition of type currently unsupported\n"
+def_ty :: IR.IRCtx -> IR.DSIRId IR.DeclSymbol -> Mangle.MangledName -> IR.Type -> String
+def_ty _ _ _ _ = "#error definition of type currently unsupported\n"
 -- decl_v {{{1
-decl_v :: IR.IRCtx -> IR.VIRId IR.Value -> IR.Value -> String
-decl_v irctx name = IR.apply_to_v (decl_fun irctx name)
+decl_v :: IR.IRCtx -> IR.VIRId IR.Value -> Mangle.MangledName -> IR.Value -> String
+decl_v irctx path mname = IR.apply_to_v (decl_fun irctx path mname)
 
-decl_fun :: IR.IRCtx -> IR.VIRId IR.Value -> IR.Function -> String
-decl_fun _ _ _ = "#error declaration of function currently unsupported\n"
+decl_fun :: IR.IRCtx -> IR.VIRId IR.Value -> Mangle.MangledName -> IR.Function -> String
+decl_fun _ _ _ _ = "#error declaration of function currently unsupported\n"
 -- def_v {{{1
-def_v :: IR.IRCtx -> IR.VIRId IR.Value -> IR.Value -> String
-def_v irctx name = IR.apply_to_v (def_fun irctx name)
+def_v :: IR.IRCtx -> IR.VIRId IR.Value -> Mangle.MangledName -> IR.Value -> String
+def_v irctx path mname = IR.apply_to_v (def_fun irctx path mname)
 
-def_fun :: IR.IRCtx -> IR.VIRId IR.Value -> IR.Function -> String
-def_fun _ _ _ = "#error declaration of function currently unsupported\n"
+def_fun :: IR.IRCtx -> IR.VIRId IR.Value -> Mangle.MangledName -> IR.Function -> String
+def_fun _ _ _ _ = "#error declaration of function currently unsupported\n"
