a572d09411a5762f0fc71cde6c35a9be29d22f4a
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-06-13 11:01:52 -0400

    point free function

--- src/Mangle/Tag.hs
+++ src/Mangle/Tag.hs
@@ -8,7 +8,7 @@ import Mangle.TagName
 data Tag = Tag (TagName TagTagName) [Tag] | StrTag (TagName StrTagName) String
 
 str_tag :: Tag -> String
-str_tag t = "_K" ++ str_tag' t
+str_tag = ("_K"++) . str_tag'
     where
         str_tag' (Tag name children) = str_tag_name name ++ show (length children) ++ concatMap str_tag' children
         str_tag' (StrTag name val) = str_tag_name name ++ show (length val) ++ "_" ++ val -- TODO: deal with non-ascii values
