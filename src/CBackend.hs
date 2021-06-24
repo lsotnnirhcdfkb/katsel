d64aa4aeed2dd0cb40d4ee482a1af50dfbeeaba6
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-06-24 17:34:38 -0400

    properly lower unit types, fix function with no parameters, fix spacing

--- src/CBackend.hs
+++ src/CBackend.hs
@@ -53,7 +53,10 @@ data CDeclarator
     | FunctionDeclarator CDeclarator [CDecl]
 
 str_cdecl :: CDecl -> String
-str_cdecl (CDecl res declarator) = res ++ " " ++ str_declarator declarator
+str_cdecl (CDecl res declarator) =
+    case str_declarator declarator of
+        "" -> res
+        s -> res ++ " " ++ s
     where
         str_declarator' d = "(" ++ str_declarator d ++ ")"
 
@@ -61,6 +64,7 @@ str_cdecl (CDecl res declarator) = res ++ " " ++ str_declarator declarator
         str_declarator AbstractDeclarator = ""
         str_declarator (PointerDeclarator decl) = "*" ++ str_declarator' decl
         str_declarator (ArrayDeclarator decl size) = str_declarator' decl ++ "[" ++ show size ++ "]"
+        str_declarator (FunctionDeclarator decl []) = str_declarator' decl ++ "(void)"
         str_declarator (FunctionDeclarator decl params) = str_declarator' decl ++ "(" ++ intercalate ", " (map str_cdecl params) ++ ")"
 -- converting types to c declarations {{{1
 type_to_cdecl' :: IR.IRCtx -> InternerIdx IR.Type -> Maybe Mangle.MangledName -> CDecl
@@ -70,31 +74,40 @@ type_to_cdecl :: IR.IRCtx -> IR.Type -> Maybe Mangle.MangledName -> CDecl
 type_to_cdecl irctx ty name = CDecl basic_type declarator
     where
         name_declarator = maybe AbstractDeclarator IdentifierDeclarator name
-        (basic_type, declarator) = convert name_declarator ty
+        (basic_type, declarator) = convert False name_declarator ty
 
-        convert' :: CDeclarator -> InternerIdx IR.Type -> (String, CDeclarator)
-        convert' parent idx = IR.apply_to_tyidx (convert parent) irctx idx
+        convert' :: Bool -> CDeclarator -> InternerIdx IR.Type -> (String, CDeclarator)
+        convert' uav parent idx = IR.apply_to_tyidx (convert uav parent) irctx idx
 
-        convert :: CDeclarator -> IR.Type -> (String, CDeclarator)
+        convert :: Bool -> CDeclarator -> IR.Type -> (String, CDeclarator)
         -- the given declarator is a value that is the same time as the type passed in
         -- this function will add to the declarator the operation that can be performed on the type, and also return the basic type
-        convert parent (IR.FloatType _ 32) = ("float", parent)
-        convert parent (IR.FloatType _ 64) = ("double", parent)
-        convert _ (IR.FloatType _ _) = error "cannot convert invalid float type to c declaration"
-        convert parent (IR.IntType _ size signedness) =
+        convert _ parent (IR.FloatType _ 32) = ("float", parent)
+        convert _ parent (IR.FloatType _ 64) = ("double", parent)
+        convert _ _ (IR.FloatType _ _) = error "cannot convert invalid float type to c declaration"
+        convert _ parent (IR.IntType _ size signedness) =
             let signedness_str = case signedness of
-                    IR.Signed -> "s"
+                    IR.Signed -> ""
                     IR.Unsigned -> "u"
             in (signedness_str ++ "int" ++ show size ++ "_t", parent)
 
-        convert parent (IR.UnitType _) = ("void", parent)
+        convert unit_as_void parent (IR.UnitType _)
+            | unit_as_void = ("void", parent)
+            | otherwise = error "cannot lower unit type to c"
 
-        convert parent (IR.CharType _) = ("uint8_t", parent) -- TODO: this maybe should not be 8 bits
-        convert parent (IR.BoolType _) = ("int", parent)
+        convert _ parent (IR.CharType _) = ("uint8_t", parent) -- TODO: this maybe should not be 8 bits
+        convert _ parent (IR.BoolType _) = ("int", parent)
 
-        convert parent (IR.FunctionPointerType _ ret params) =
-            let parent' = FunctionDeclarator (PointerDeclarator parent) (map (IR.apply_to_tyidx (\ pty -> type_to_cdecl irctx pty Nothing) irctx) params)
-            in convert' parent' ret
+        convert _ parent (IR.FunctionPointerType _ ret params) =
+            let is_unit = IR.apply_to_tyidx
+                    (\case
+                        IR.UnitType _ -> True
+                        _ -> False
+                    ) irctx
+                cparams = filter (not . is_unit) params
+
+                parent' = FunctionDeclarator (PointerDeclarator parent) (map (IR.apply_to_tyidx (\ pty -> type_to_cdecl irctx pty Nothing) irctx) cparams)
+            in convert' True parent' ret
 
         -- convert parent (IR.PointerType _ pointee) =
             -- let parent' = PointerDeclarator parent
