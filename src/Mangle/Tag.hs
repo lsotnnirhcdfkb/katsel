23d659e297fa300da920ed94920d2bc79fcfc2f9
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-06-24 16:34:27 -0400

    encode non-identifier characters in mangler

--- src/Mangle/Tag.hs
+++ src/Mangle/Tag.hs
@@ -5,10 +5,25 @@ module Mangle.Tag
 
 import Mangle.TagName
 
+import Numeric(showHex)
+import Data.Char(isAscii, isAlpha, isDigit, ord)
+
 data Tag = Tag (TagName TagTagName) [Tag] | StrTag (TagName StrTagName) String
 
 str_tag :: Tag -> String
 str_tag = ("_K"++) . str_tag'
     where
         str_tag' (Tag name children) = str_tag_name name ++ show (length children) ++ concatMap str_tag' children
-        str_tag' (StrTag name val) = str_tag_name name ++ show (length val) ++ "_" ++ val -- TODO: deal with non-ascii values
+        str_tag' (StrTag name val) =
+            let val' = encode val
+            in str_tag_name name ++ show (length val') ++ val' -- TODO: deal with non-ascii values
+
+encode :: String -> String
+encode "" = ""
+encode (c:cs) = encode_ch True c ++ concatMap (encode_ch False) cs
+    where
+        encode_ch first ch
+            | isAlpha ch && isAscii ch = [ch]
+            | isDigit ch && isAscii ch && not first = [ch]
+            | ch == '_' = "__"
+            | otherwise = "_u" ++ showHex (ord ch) "_"
