2d70f2558ecc162316c9470cfc29661573e49a42
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-06-26 16:32:59 -0400

    fix source-dirs for tests, remove empty test

--- test/test.hs
+++ test/test.hs
@@ -3,10 +3,4 @@ module Main where
 import Test
 
 main :: IO ()
-main = run_test_suite $ TestSuite
-    [ Describe "thing"
-        [ When "given nothing"
-            [ ItCan "do something" True
-            ]
-        ]
-    ]
+main = run_test_suite $ TestSuite []
