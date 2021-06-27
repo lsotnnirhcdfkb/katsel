439c12aa6cea55f01e1d109195d246b8ca35b706
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-06-27 00:07:21 -0400

    count test results

--- test/Test.hs
+++ test/Test.hs
@@ -8,7 +8,7 @@ module Test
 
 -- inspired by RSpec
 
-import Control.Monad(unless)
+import Control.Monad(when)
 
 import System.Exit(exitFailure)
 
@@ -22,10 +22,31 @@ data TestResult = Pass | Fail String | Untested
 
 run_test_suite :: TestSuite -> IO ()
 run_test_suite (TestSuite tests) =
-    run_test_list 0 tests >>= \ success ->
-    unless success exitFailure
+    run_test_list 0 tests >>= \ results ->
+    let count b = length $ filter b results
 
-run_test :: Int -> Test -> IO Bool
+        match_pass = \case
+            Pass -> True
+            _ -> False
+
+        match_fail = \case
+            Fail _ -> True
+            _ -> False
+
+        match_untested = \case
+            Untested -> True
+            _ -> False
+
+        num_pass = count match_pass
+        num_fail = count match_fail
+        num_untested = count match_untested
+
+    in putStrLn (show num_pass ++ " tests passed") >>
+    putStrLn (show num_fail ++ " tests failed") >>
+    putStrLn (show num_untested ++ " tests untested") >>
+    when (num_fail > 0) exitFailure
+
+run_test :: Int -> Test -> IO [TestResult]
 
 run_test indent_amt (Describe name test_list) =
     indent_put_str indent_amt (name ++ ":\n") >>
@@ -37,10 +58,13 @@ run_test indent_amt (When context test_list) =
 
 run_test indent_amt (ItCan action result) =
     indent_put_str indent_amt "it " >>
-    result >>= \case
-        Pass -> putStr ("can " ++ action) >> return True
-        Fail msg -> putStr ("cannot " ++ action ++ ": " ++ msg) >> return False
-        Untested -> putStr ("maybe can " ++ action) >> return True
+    result >>= \ result' ->
+    (case result' of
+        Pass -> putStr ("can " ++ action)
+        Fail msg -> putStr ("cannot " ++ action ++ ": " ++ msg)
+        Untested -> putStr ("maybe can " ++ action)
+    ) >>
+    return [result']
 
 indent :: Int -> String
 indent = (`replicate` ' ')
@@ -48,5 +72,5 @@ indent = (`replicate` ' ')
 indent_put_str :: Int -> String -> IO ()
 indent_put_str amt = putStr . (indent amt ++)
 
-run_test_list :: Int -> [Test] -> IO Bool
-run_test_list indent_amt test_list = and <$> mapM (run_test indent_amt) test_list
+run_test_list :: Int -> [Test] -> IO [TestResult]
+run_test_list indent_amt test_list = concat <$> mapM (run_test indent_amt) test_list
