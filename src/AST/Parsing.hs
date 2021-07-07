f57763e77b0e3bb9731b2c4bf71b9626155e036b
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-07-07 15:36:15 -0400

    add tests for parsing functions except for combinators

--- src/AST/Parsing.hs
+++ src/AST/Parsing.hs
@@ -1,8 +1,13 @@
 module AST.Parsing
     ( parse_from_toks
     , ParseError
+
+    , tests
     ) where
 
+import Test
+
+import File
 import Location
 import MonadUtils
 import MaybeWithError
@@ -21,6 +26,8 @@ import Control.Monad.State.Lazy (State, state, runState)
 -- ParseError {{{1
 data ParseError
     = Expected String Span
+    | TestError
+    | TestErrorWithSpan Span
 
 type ParseErrorSpanPending = Span -> ParseError
 
@@ -49,6 +56,12 @@ type SynchronizationPredicate = ParserPredicate
 type StopPredicate = ParserPredicate
 
 type TokenPredicate = Located Tokens.Token -> Bool
+-- run_parse_fun {{{1
+run_parse_fun :: ParseFun a -> [Located Tokens.Token] -> ([ParseError], a)
+run_parse_fun pf tokens =
+    let parser = Parser [] tokens Nothing
+        (res, Parser errs _ _) = runState pf parser
+    in (reverse errs, res)
 -- error helpers {{{1
 add_error :: ParseError -> ParseFun (ErrorLoggedPromise ParseError)
 add_error err = state $ \ parser -> (error_logged_promise err, parser { get_parse_errors = err : get_parse_errors parser })
@@ -66,12 +79,6 @@ constr_eq a b = toConstr a == toConstr b
 
 is_tt :: Tokens.Token -> TokenPredicate
 is_tt a (Located _ b) = constr_eq a b
-
-is_tt_u :: Tokens.Token -> Located Tokens.Token -> Maybe ()
-is_tt_u a b = if is_tt a b then Just () else Nothing
-
-is_tt_s :: Tokens.Token -> Located Tokens.Token -> Maybe Span
-is_tt_s a b@(Located sp _) = if is_tt a b then Just sp else Nothing
 
 is_at_end :: ParserPredicate
 is_at_end = peek_matches_pred $ is_tt Tokens.EOF
@@ -135,9 +142,9 @@ consume predicate =
 
 consume_or_error :: ParseErrorSpanPending -> TokenPredicate -> ParseFunMWE (Located Tokens.Token)
 consume_or_error err predicate =
-    peek_matches predicate >>= \case
-        True -> JustWithError <$> advance
-        False -> peek >>= \ (Located sp _) -> add_error_and_nothing (err sp)
+    consume predicate >>= \case
+        Just tok -> return $ JustWithError tok
+        Nothing -> peek_span >>= add_error_and_nothing . err
 
 synchronize :: SynchronizationPredicate -> ParseFun ()
 synchronize sync_predicate = advance >> go
@@ -266,7 +273,371 @@ parse_module =
     return (Located (span_from_list decl_list eof_span) (DModule' decl_list))
 -- parsing entry point{{{1
 parse_from_toks :: [Located Tokens.Token] -> ([ParseError], LDModule)
-parse_from_toks toks =
-    let parser = Parser [] toks Nothing
-        (res, Parser errs _ _) = runState parse_module parser
-    in (reverse errs, res)
+parse_from_toks = run_parse_fun parse_module
+-- tests {{{1
+tests :: Test
+tests =
+    DescribeModule "AST.Parsing"
+        [ DescribeFunction "add_error"
+            [ ItCan "add an error and return a promise" $
+                let (errs, promise) = run_parse_fun (add_error TestError) []
+                in case errs of
+                    [TestError] -> seq promise pass_test -- if promise does not throw, it is not undefined and is an actual promise
+                    _ -> fail_test
+            ]
+
+        , DescribeFunction "add_error_and_nothing"
+            [ ItCan "add an error and return NothingWithError" $
+                let res = run_parse_fun (add_error_and_nothing TestError) []
+                in case res of
+                    ([TestError], NothingWithError _) -> pass_test
+                    _ -> fail_test
+            ]
+
+        , DescribeFunction "span_from_list"
+            [ ItCan "join the span of the first and last elements of a list of located items" $
+                let fake_file = make_file "fake_file" "a b c d e f"
+
+                    span1 = Span start before end
+                        where
+                            start = make_location_from_ind fake_file 0
+                            before = make_location_from_ind fake_file 3
+                            end = make_location_from_ind fake_file 4
+                    span2 = Span start before end
+                        where
+                            start = make_location_from_ind fake_file 5
+                            before = make_location_from_ind fake_file 6
+                            end = make_location_from_ind fake_file 7
+
+                    things = [Located span1 (), undefined, Located span2 ()]
+                -- TODO: use pass_if
+                in if span_from_list things undefined == (span1 `join_span` span2)
+                    then pass_test
+                    else fail_test
+
+            , ItCan "use the fallback span if there is an empty list" $
+                let fake_file = make_file "fake_file" "a b c d e f"
+                    sp = Span start before end
+                        where
+                            start = make_location_from_ind fake_file 0
+                            before = make_location_from_ind fake_file 1
+                            end = make_location_from_ind fake_file 2
+                in if span_from_list [] sp == sp
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "constr_eq"
+            [ ItCan "return true if the two arguments are of the same constructor" $
+                if constr_eq (Just ()) (Just 2 :: Maybe Int)
+                    then pass_test
+                    else fail_test
+
+            , ItCan "return false if the two arguments are not of the same constructor" $
+                if not $ constr_eq (Just ()) (Nothing :: Maybe ())
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "is_tt"
+            [ ItCan "tell whether a token is a certain token type" $
+                if is_tt (Tokens.Identifier "") (Located undefined $ Tokens.Identifier "abc")
+                    then pass_test
+                    else fail_test
+
+            , ItCan "tell whether a token is not a certain token type" $
+                if not $ is_tt Tokens.OParen (Located undefined Tokens.CParen)
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "is_at_end"
+            [ ItCan "tell if the next token is an EOF token" $
+                let (_, pred_result) = run_parse_fun (parser_matches is_at_end) [Located undefined Tokens.EOF]
+                in if pred_result
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "predicate_or"
+            [ ItCan "combine predicates with an or" $
+                let combined = predicate_or odd even
+                in if combined (2 :: Int) && combined 3
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "predicate_and"
+            [ ItCan "combine predicates with an and" $
+                let combined = predicate_and odd even
+                in if not (combined (2 :: Int)) && not (combined 3)
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "peek_matches_pred"
+            [ ItCan "convert a token predicate into a parser predicate" $
+                let (_, pred_result) = run_parse_fun (parser_matches $ peek_matches_pred (is_tt Tokens.OParen)) [Located undefined Tokens.OParen]
+                in if pred_result
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "peek"
+            [ ItCan "return the next token" $
+                let fake_file = make_file "fake_file" "a"
+
+                    sp = Span start before end
+                        where
+                            start = make_location_from_ind fake_file 0
+                            before = make_location_from_ind fake_file 0
+                            end = make_location_from_ind fake_file 1
+
+                    tok = Located sp Tokens.OParen
+                    (_, res) = run_parse_fun peek [tok]
+                in if res == tok
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "peek_span"
+            [ ItCan "return the next token's span" $
+                let fake_file = make_file "fake_file" "a"
+
+                    sp = Span start before end
+                        where
+                            start = make_location_from_ind fake_file 0
+                            before = make_location_from_ind fake_file 0
+                            end = make_location_from_ind fake_file 1
+
+                    (_, res) = run_parse_fun peek_span [Located sp undefined]
+                in if res == sp
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "peek_matches"
+            [ ItCan "check if the next token matches a predicate" $
+                let (_, pred_result) = run_parse_fun (peek_matches (is_tt Tokens.OParen)) [Located undefined Tokens.OParen]
+                in if pred_result
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "peek_match"
+            [ ItCan "check which predicate is matched by the next token, and then run the appropriate action" $
+                let predicates =
+                        [ (is_tt Tokens.OParen, return 0)
+                        , (is_tt Tokens.CParen, return 1)
+                        ]
+                    fallback = return 2
+
+                    (_, res) = run_parse_fun (peek_match predicates fallback) [Located undefined Tokens.OParen]
+                in if res == (0 :: Int)
+                    then pass_test
+                    else fail_test
+
+
+            , ItCan "use the fallback action if none of the predicates match" $
+                let (_, res) = run_parse_fun (peek_match [] (return 1)) [Located undefined undefined]
+                in if res == (1 :: Int)
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "parser_matches"
+            [ ItCan "check that a parser matches a predicate" $
+                let (_, res) = run_parse_fun (parser_matches is_at_end) [Located undefined Tokens.EOF]
+                in if res
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "advance"
+            [ ItCan "advance the parser to the next token" $
+                let (_, res) = run_parse_fun (advance >> parser_matches is_at_end) [Located undefined undefined, Located undefined Tokens.EOF]
+                in if res
+                    then pass_test
+                    else fail_test
+            -- TODO: it can return an error when advancing past the EOF token
+            ]
+
+        , DescribeFunction "advance_match"
+            [ ItCan "run the action that is associated with the predicate that matched" $
+                let predicates =
+                        [ (is_tt Tokens.OParen, undefined)
+                        , (is_tt Tokens.CParen, \ (Located _ Tokens.CParen) -> return 1)
+                        ]
+                    fallback = undefined
+
+                    (_, res) = run_parse_fun (advance_match predicates fallback) [Located undefined Tokens.CParen, Located undefined Tokens.EOF]
+
+                in if res == (1 :: Int)
+                    then pass_test
+                    else fail_test
+
+            , ItCan "advance the parser" $
+                let predicates =
+                        [ (is_tt Tokens.OParen, \ _ -> return ())
+                        ]
+                    fallback = undefined
+
+                    (_, res) = run_parse_fun (advance_match predicates fallback >> parser_matches is_at_end) [Located undefined Tokens.OParen, Located undefined Tokens.EOF]
+
+                in if res
+                    then pass_test
+                    else fail_test
+
+            , ItCan "keep the parser as is when no predicdate matches, and run the fallback action" $
+                let action = advance_match [] (return 2) >>= \ r -> parser_matches is_at_end >>= \ b -> return (r, b)
+                    (_, (match_res, end_res)) = run_parse_fun action [Located undefined Tokens.OParen, Located undefined Tokens.EOF]
+                in if match_res == (2 :: Int) && not end_res
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "consume"
+            [ ItCan "consume the next token if it matches the predicate" $  
+                let fake_file = make_file "fake_file" "a"
+                    sp = Span start before end
+                        where
+                            start = make_location_from_ind fake_file 0
+                            before = make_location_from_ind fake_file 0
+                            end = make_location_from_ind fake_file 1
+
+                    action =
+                        consume (is_tt Tokens.OParen) >>= \ t ->
+                        parser_matches is_at_end >>= \ a ->
+                        return (t, a)
+
+                    (_, (tok, at_end)) = run_parse_fun action [Located sp Tokens.OParen, Located undefined Tokens.EOF]
+                in if tok == Just (Located sp Tokens.OParen) && at_end
+                    then pass_test
+                    else fail_test
+
+            , ItCan "do nothing if the next token does not match the predicate" $
+                let fake_file = make_file "fake_file" "a"
+                    sp = Span start before end
+                        where
+                            start = make_location_from_ind fake_file 0
+                            before = make_location_from_ind fake_file 0
+                            end = make_location_from_ind fake_file 1
+
+                    action =
+                        consume (is_tt Tokens.OParen) >>= \ t ->
+                        peek >>= \ nt ->
+                        return (t, nt)
+
+                    (_, (tok, next_token)) = run_parse_fun action [Located sp Tokens.CParen]
+
+                in if tok == Nothing && next_token == Located sp Tokens.CParen
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "consume_or_error"
+            [ ItCan "consume the next token if it matches the predicate" $  
+                let fake_file = make_file "fake_file" "a"
+                    sp = Span start before end
+                        where
+                            start = make_location_from_ind fake_file 0
+                            before = make_location_from_ind fake_file 0
+                            end = make_location_from_ind fake_file 1
+
+                    action =
+                        consume_or_error TestErrorWithSpan (is_tt Tokens.OParen) >>= \ t ->
+                        parser_matches is_at_end >>= \ a ->
+                        return (t, a)
+
+                    (_, (tok, at_end)) = run_parse_fun action [Located sp Tokens.OParen, Located undefined Tokens.EOF]
+                in case tok of
+                    JustWithError (Located sp' Tokens.OParen)
+                        | sp == sp' && at_end -> pass_test
+                    _ -> fail_test
+
+            , ItCan "do add an error if the next token does not match the predicate" $
+                let fake_file = make_file "fake_file" "a"
+                    sp = Span start before end
+                        where
+                            start = make_location_from_ind fake_file 0
+                            before = make_location_from_ind fake_file 0
+                            end = make_location_from_ind fake_file 1
+
+                    action =
+                        consume_or_error TestErrorWithSpan (is_tt Tokens.OParen) >>= \ t ->
+                        peek >>= \ nt ->
+                        return (t, nt)
+
+                    (errs, (tok, next_token)) = run_parse_fun action [Located sp Tokens.CParen]
+
+                in case (errs, tok) of
+                    ([TestErrorWithSpan _], NothingWithError _)
+                        | next_token == Located sp Tokens.CParen -> pass_test
+                    _ -> fail_test
+            ]
+
+        , DescribeFunction "synchronize"
+            [ ItCan "skip tokens until the parser matches a predicate" $
+                let action =
+                        synchronize (peek_matches_pred $ is_tt Tokens.CParen) >>
+                        peek
+
+                    toks =
+                        [ Located undefined Tokens.OParen
+                        , Located undefined Tokens.OParen
+                        , Located undefined Tokens.OParen
+                        , Located undefined Tokens.OParen
+                        , Located undefined Tokens.OParen
+                        , Located undefined Tokens.CParen
+                        ]
+
+                    (_, next_token) = run_parse_fun action toks
+                in case next_token of
+                    Located _ Tokens.CParen -> pass_test
+                    _ -> fail_test
+
+            , ItCan "stop at an EOF token" $
+                let action =
+                        synchronize (const False) >>
+                        parser_matches is_at_end
+
+                    toks =
+                        [ Located undefined Tokens.OParen
+                        , Located undefined Tokens.OParen
+                        , Located undefined Tokens.OParen
+                        , Located undefined Tokens.OParen
+                        , Located undefined Tokens.OParen
+                        , Located undefined Tokens.CParen
+                        , Located undefined Tokens.EOF
+                        ]
+
+                    (_, at_end) = run_parse_fun action toks
+                in if at_end
+                    then pass_test
+                    else fail_test
+            ]
+
+        , DescribeFunction "thing_list_no_separator" []
+
+        , DescribeFunction "thing_list_with_separator" []
+
+        , DescribeFunction "line_ending" []
+
+        , DescribeFunction "optional_line_ending" []
+
+        , DescribeFunction "parse_type" []
+
+        , DescribeFunction "parse_type_annotation" []
+
+        , DescribeFunction "parse_param" []
+
+        , DescribeFunction "parse_block" []
+
+        , DescribeFunction "parse_decl" []
+
+        , DescribeFunction "parse_fun" []
+
+        , DescribeFunction "parse_module" []
+
+        , DescribeFunction "parse_from_toks" []
+        ]
--- test/test.hs
+++ test/test.hs
@@ -3,6 +3,7 @@ module Main where
 import Test
 
 import Tokens (tests)
+import AST.Parsing (tests)
 
 main :: IO ()
-main = run_test_suite $ TestSuite [tests]
+main = run_test_suite $ TestSuite [Tokens.tests, AST.Parsing.tests]
